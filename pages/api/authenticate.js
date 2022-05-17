d9f87930e51f3f7098b00b4a2116d14f19a631a0
Martin Makarsky <martin.makarsky@fingerprintjs.com>
2022-05-17 08:39:15 +0200

    feat: refactor rules

--- pages/api/authenticate.js
+++ pages/api/authenticate.js
@@ -10,7 +10,7 @@ const sequelize = new Sequelize('database', '', '', {
   logging: false,
 });
 
-// Defines db model for login attempt
+// Defines db model for login attempt.
 const LoginAttempt = sequelize.define('login-attempt', {
   visitorId: {
     type: Sequelize.STRING,
@@ -34,7 +34,7 @@ const ourOrigins = [
   'https://nextjs-dmv5c7--3000.local.webcontainer.io',
   'https://jellyfish-app-cnbob.ondigitalocean.app',
   'https://localhost:3000',
-  'http://localhost:3000'
+  'http://localhost:3000',
 ];
 
 // Mocked user with leaked credentials asociated with visitorIds.
@@ -48,88 +48,12 @@ const mockedUser = {
   ],
 };
 
-// TODO: Fix returning from internal functions, middleware handlers maybe?
-export default async (req, res) => {
-  // This API route accepts only POST requests.
-  if (req.method !== 'POST') {
-    res.status(405).send({ message: 'Only POST requests allowed' });
-    return;
+class CheckResult {
+  constructor(message, messageSeverity, type) {
+    this.message = message;
+    this.messageSeverity = messageSeverity;
+    this.type = type;
   }
-
-  res.setHeader('Content-Type', 'application/json');
-
-  // Get requestId and visitorId from the client.
-  const visitorId = req.body.visitorId;
-  const requestId = req.body.requestId;
-  const userName = req.body.userName;
-  const password = req.body.password;
-
-  // Information from the client side might have been tampered.
-  // It's a best practice to validate provided information with Server API. It is recommended to use the requestId and visitorId pair.
-  const visitorData = await getVisitorData(visitorId, requestId);
-
-  // Check if the Server API contains info about this specific identification request.
-  // If not, the request might have been edited and we don't trust this identification attempt.
-  await verifyIdentificationRequestFromSeverApi(req, res, visitorData, userName);
-
-  // The Confidence Score reflects the system's degree of certainty that the visitor identifier is correct.
-  // If it's lower than the certain threshold we recommend using an additional way of verification, e.g. 2FA or email.
-  // More info: https://dev.fingerprintjs.com/docs/understanding-your-confidence-score
-  await checkConfidenceScore(req, res, visitorData, userName);
-
-  // An attacker might have acquired valid requestId and visitorId via phishing.
-  // It's recommended to check age of the identification request with the Server API.
-  await checkAgeOfIdentification(req, res, visitorData, userName);
-
-  // Get all unsuccessful attempts during the last 24 hours.
-  // If the visitorId performed 5 unsuccessful login attempts during the last 24 hours we do not perform login and we notify the user with the next steps according to internal processes.
-  // The count of attempts and time window might vary.
-  await checkUnsuccessfulIdentifications(req, res, visitorData, userName);
-
-  // Check if the authentication request comes from the same IP adress as identification request.
-  // This check is disabled on purpose in the Stackblitz environment.
-  // await checkIpAddressIntegrity(req, res, visitorData, userName);
-
-  // Check if the authentication request comes from the known origin
-  // Check if the authentication request's origin corresponds to the origin/URL provided by the FingerprintJSPro Server API.
-  // Additionally, one should set Request Filtering settings in the dashboard: https://dev.fingerprintjs.com/docs/request-filtering
-  await checkOriginsIntegrity(req, res, visitorData, userName);
-
-  // Check if the provided credentials are correct.
-  // If they provided valid credentials but they never logged in using this visitorId, we recommend using an additional way of verification, e.g. 2FA or email.
-  await checkCredentialsAndKnownVisitorIds(req, res, visitorData, userName, password);
-};
-
-// Every identification request should be validated using FingerprintJS Pro Server API.
-// Alternatively, on the Node.js environment one can use Server API Node.js library: https://github.com/fingerprintjs/fingerprintjs-pro-server-api-node-sdk
-// const client = new FingerprintJsServerApiClient({
-//   region: Region.Global,
-//   apiKey: 'F6gQ8H8vQLc7mVsVKaFx',
-//   authenticationMode: AuthenticationMode.QueryParameter,
-// });
-
-// const serverApiFilter = { request_id: requestId };
-// const visitorData = await client.getVisitorHistory(
-//   visitorId,
-//   serverApiFilter
-// );
-// return visitorData;
-async function getVisitorData(visitorId, requestId) {
-  const fingerprintJSProServerApiUrl = new URL(
-    `https://api.fpjs.io/visitors/${visitorId}`
-  );
-
-  fingerprintJSProServerApiUrl.searchParams.append(
-    'api_key',
-    'F6gQ8H8vQLc7mVsVKaFx' // In the real world use-case, we recommend using Auth-API-Key header instead: https://dev.fingerprintjs.com/docs/server-api#api-methods. Api key should be stored in the environment variables/secrets.
-  );
-  fingerprintJSProServerApiUrl.searchParams.append('request_id', requestId);
-
-  const visitorServerApiResponse = await fetch(
-    fingerprintJSProServerApiUrl.href
-  );
-
-  return await visitorServerApiResponse.json();
 }
 
 const loginAttemptResult = Object.freeze({
@@ -144,54 +68,135 @@ const loginAttemptResult = Object.freeze({
   Passed: 'Passed',
 });
 
-async function verifyIdentificationRequestFromSeverApi(req, res, visitorData, userName) {
+const messageSeverity = Object.freeze({
+  Success: 'success',
+  Warning: 'warning',
+  Error: 'error',
+});
+
+async function login(req, res, ruleChecks) {
+  // Get requestId and visitorId from the client.
+  const visitorId = req.body.visitorId;
+  const requestId = req.body.requestId;
+  const userName = req.body.userName;
+
+  // Information from the client side might have been tampered.
+  // It's best practice to validate provided information with the Server API.
+  // It is recommended to use the requestId and visitorId pair.
+  const visitorData = await getVisitorData(visitorId, requestId);
+
+  for (const ruleCheck of ruleChecks) {
+    const result = await ruleCheck(visitorData, req);
+
+    if (result) {
+      await logLoginAttempt(visitorId, userName, result.type);
+
+      switch (result.type) {
+        case loginAttemptResult.Passed:
+        case loginAttemptResult.Challenged:
+          console.log(JSON.stringify(result));
+          return getOkReponse(res, result.message, result.messageSeverity);
+        default:
+          reportSuspiciousActivity(req);
+          console.log(JSON.stringify(result));
+
+          return getForbiddenReponse(res, result.message, result.messageSeverity);
+      }
+    }
+  }
+}
+
+export default async (req, res) => {
+  // This API route accepts only POST requests.
+  if (req.method !== 'POST') {
+    res.status(405).send({ message: 'Only POST requests allowed' });
+    return;
+  }
+  res.setHeader('Content-Type', 'application/json');
+
+  return await login(req, res, [
+    checkFreshIdentificationRequest,
+    checkConfidenceScore,
+    checkUnsuccessfulIdentifications,
+    // checkIpAddressIntegrity, // This check is disabled on purpose in the Stackblitz environment.
+    checkOriginsIntegrity,
+    checkCredentialsAndKnownVisitorIds,
+  ]);
+};
+
+// Every identification request should be validated using the FingerprintJS Pro Server API.
+
+// Alternatively, on the Node.js environment one can use Server API Node.js library: https://github.com/fingerprintjs/fingerprintjs-pro-server-api-node-sdk
+// const client = new FingerprintJsServerApiClient({
+//   region: Region.Global,
+//   apiKey: 'F6gQ8H8vQLc7mVsVKaFx',
+//   authenticationMode: AuthenticationMode.QueryParameter,
+// });
+
+// const serverApiFilter = { request_id: requestId };
+// const visitorData = await client.getVisitorHistory(
+//   visitorId,
+//   serverApiFilter
+// );
+// return visitorData;
+
+async function getVisitorData(visitorId, requestId) {
+  const fingerprintJSProServerApiUrl = new URL(
+    `https://api.fpjs.io/visitors/${visitorId}`
+  );
+
+  fingerprintJSProServerApiUrl.searchParams.append(
+    'api_key',
+    // In a real world use-case, we recommend using Auth-API-Key header instead: https://dev.fingerprintjs.com/docs/server-api#api-methods.
+    // The API key should be stored in the environment variables/secrets.
+    'F6gQ8H8vQLc7mVsVKaFx'
+  );
+  fingerprintJSProServerApiUrl.searchParams.append('request_id', requestId);
+
+  const visitorServerApiResponse = await fetch(
+    fingerprintJSProServerApiUrl.href
+  );
+
+  return await visitorServerApiResponse.json();
+}
+
+function checkFreshIdentificationRequest(visitorData) {
+  // The Server API must contain information about this specific identification request.
+  // If not, the request might have been tampered with and we don't trust this identification attempt.
   if (visitorData.visits.length === 0) {
-    reportSuspiciousActivityAccordintInternalProcesses(req);
-    await logLoginAttempt(
-      visitorData.visitorId,
-      userName,
+    return new CheckResult(
+      'Hmmm, sneaky trying to forge information from the client-side, no luck this time, no login attempt was performed.',
+      messageSeverity.Error,
       loginAttemptResult.RequestIdMissmatch
     );
-    return getForbiddenReponse(
-      res,
-      'Hmmm, sneaky trying to forge information from the client-side, no luck this time, no login attempt was performed.'
-    );
   }
-}
 
-async function checkConfidenceScore(req, res, visitorData, userName) {
-  if (visitorData.visits[0].confidence.score < 0.98) {
-    reportSuspiciousActivityAccordintInternalProcesses(req);
-    await logLoginAttempt(
-      visitorData.visitorId,
-      userName,
-      loginAttemptResult.LowConfidenceScore
-    );
-
-    return getForbiddenReponse(
-      res,
-      "Low confidence score, we'd rather verify you with the second factor,"
-    );
-  }
-}
-
-async function checkAgeOfIdentification(req, res, visitorData, userName) {
-  if (new Date().getTime() - visitorData.visits[0].timestamp > 2000) {
-    reportSuspiciousActivityAccordintInternalProcesses(req);
-    await logLoginAttempt(
-      visitorData.visitorId,
-      userName,
+  // An attacker might have acquired a valid requestId and visitorId via phishing.
+  // It's recommended to check freshness of the identification request to prevent replay attacks.
+  if (new Date().getTime() - visitorData.visits[0].timestamp > 3000) {
+    return new CheckResult(
+      'Old requestId detected. Login attempt ignored and logged.',
+      messageSeverity.Error,
       loginAttemptResult.OldTimestamp
     );
+  }
+}
 
-    return getForbiddenReponse(
-      res,
-      'Old requestId detected. Login attempt ignored and logged.'
+function checkConfidenceScore(visitorData) {
+  // The Confidence Score reflects the system's degree of certainty that the visitor identifier is correct.
+  // If it's lower than the certain threshold we recommend using an additional way of verification, e.g. 2FA or email.
+  // More info: https://dev.fingerprintjs.com/docs/understanding-your-confidence-score
+  if (visitorData.visits[0].confidence.score < 0.98) {
+    return new CheckResult(
+      "Low confidence score, we'd rather verify you with the second factor,",
+      messageSeverity.Error,
+      loginAttemptResult.LowConfidenceScore
     );
   }
 }
 
-async function checkUnsuccessfulIdentifications(req, res, visitorData, userName) {
+async function checkUnsuccessfulIdentifications(visitorData) {
+  // Gets all unsuccessful attempts during the last 24 hours.
   const visitorLoginAttemptCountQueryResult =
     await LoginAttempt.findAndCountAll({
       where: {
@@ -207,97 +212,80 @@ async function checkUnsuccessfulIdentifications(req, res, visitorData, userName)
       },
     });
 
-  if (visitorLoginAttemptCountQueryResult.count > 4) {
-    reportSuspiciousActivityAccordintInternalProcesses(req);
-    await logLoginAttempt(
-      visitorData.visitorId,
-      userName,
+  // If the visitorId performed 5 unsuccessful login attempts during the last 24 hours we do not perform the login.
+  // The count of attempts and time window might vary.
+  if (visitorLoginAttemptCountQueryResult.count > 5) {
+    return new CheckResult(
+      'You had more than 5 attempts during the last 24 hours. This login attempt was not performed.',
+      messageSeverity.Error,
       loginAttemptResult.TooManyAttempts
     );
-
-    return getForbiddenReponse(
-      res,
-      'You had more than 5 attempts during the last 24 hours. This login attempt was not performed.'
-    );
   }
 }
 
-async function checkIpAddressIntegrity(req, res, visitorData, userName) {
+function checkIpAddressIntegrity(visitorData, request) {
+  // Checks if the authentication request comes from the same IP adress as the identification request.
   if (
-    req.headers['x-forwarded-for'].split(',')[0] !== visitorData.visits[0].ip
+    // This is an example of obtaining the client IP address.
+    // In most cases, it's a good idea to look for the right-most external IP address in the list to prevent spoofing.
+    request.headers['x-forwarded-for'].split(',')[0] !==
+    visitorData.visits[0].ip
   ) {
-    reportSuspiciousActivityAccordintInternalProcesses(req);
-    await logLoginAttempt(
-      visitorData.visitorId,
-      userName,
+    return new CheckResult(
+      'IP mismatch. An attacker might have tried to phish the victim.',
+      messageSeverity.Error,
       loginAttemptResult.IpMismatch
     );
-
-    return getForbiddenReponse(
-      res,
-      'IP mismatch. An attacker might have tried to phish the victim.'
-    );
   }
 }
 
-async function checkOriginsIntegrity(req, res, visitorData, userName) {
+function checkOriginsIntegrity(visitorData, request) {
+  // Checks if the authentication request comes from a known origin and
+  // if the authentication request's origin corresponds to the origin/URL provided by the FingerprintJSPro Server API.
+  // Additionally, one should set Request Filtering settings in the dashboard: https://dev.fingerprintjs.com/docs/request-filtering
   const visitorDataOrigin = new URL(visitorData.visits[0].url).origin;
   if (
-    // visitorDataOrigin !== req.headers['origin'] || // This check is commented out because of different origins on the Stackblitz environment
+    // visitorDataOrigin !== request.headers['origin'] || // This check is commented out because of different origins on the Stackblitz environment
     !ourOrigins.includes(visitorDataOrigin) ||
-    !ourOrigins.includes(req.headers['origin'])
+    !ourOrigins.includes(request.headers['origin'])
   ) {
-    reportSuspiciousActivityAccordintInternalProcesses(req);
-    await logLoginAttempt(
-      visitorData.visitorId,
-      userName,
+    return new CheckResult(
+      'Origin mismatch. An attacker might have tried to phish the victim.',
+      messageSeverity.Error,
       loginAttemptResult.ForeignOrigin
-    );
-
-    return getForbiddenReponse(
-      res,
-      'Origin mismatch. An attacker might have tried to phish the victim.'
     );
   }
 }
 
-async function checkCredentialsAndKnownVisitorIds(req, res, visitorData, userName, password) {
-  if (areCredentialsCorrect(userName, password)) {
+async function checkCredentialsAndKnownVisitorIds(visitorData, request) {
+  // Checks if the provided credentials are correct.
+  if (areCredentialsCorrect(request.body.userName, request.body.password)) {
     if (
       isLoggingInFromKnownDevice(
         visitorData.visitorId,
         mockedUser.knownVisitorIds
       )
     ) {
-      await logLoginAttempt(
-        visitorData.visitorId,
-        userName,
+      return new CheckResult(
+        'We logged you in successfully.',
+        messageSeverity.Success,
         loginAttemptResult.Passed
       );
-
-      return getOkReponse(res, 'We logged you in successfully.', "success");
+      // If they provided valid credentials but they never logged in using this visitorId,
+      // we recommend using an additional way of verification, e.g. 2FA or email.
     } else {
-      await logLoginAttempt(
-        visitorData.visitorId,
-        userName,
-        loginAttemptResult.Challenged
-      );
-
-      return getOkReponse(
-        res,
+      return new CheckResult(
         "Provided credentials are correct but we've never seen you logging in using this device. Proof your identify with a second factor.",
-        "warning"
+        messageSeverity.Warning,
+        loginAttemptResult.Challenged
       );
     }
   } else {
-    reportSuspiciousActivityAccordintInternalProcesses(req);
-    await logLoginAttempt(
-      visitorData.visitorId,
-      userName,
+    return new CheckResult(
+      'Incorrect credentials, try again.',
+      messageSeverity.Error,
       loginAttemptResult.IncorrectCredentials
     );
-
-    return getForbiddenReponse(res, 'Incorrect credentials, try again.');
   }
 }
 
@@ -313,7 +301,7 @@ function isLoggingInFromKnownDevice(providedVisitorId, knownVisitorIds) {
 
 // Report suspicious user activity according to internal processes here.
 // Possibly this action could also lock the user's account temporarily.
-function reportSuspiciousActivityAccordintInternalProcesses(context) { }
+function reportSuspiciousActivity(context) { }
 
 // Persists login attempt to the database.
 async function logLoginAttempt(visitorId, userName, loginAttemptResult) {
@@ -326,10 +314,10 @@ async function logLoginAttempt(visitorId, userName, loginAttemptResult) {
   await sequelize.sync();
 }
 
-function getOkReponse(res, message, severity) {
-  return res.status(200).json({ message, severity });
+function getOkReponse(res, message, messageSeverity) {
+  return res.status(200).json({ message, severity: messageSeverity });
 }
 
-function getForbiddenReponse(res, message) {
-  return res.status(403).json({ message, severity: 'error' });
+function getForbiddenReponse(res, message, messageSeverity) {
+  return res.status(403).json({ message, severity: messageSeverity });
 }
