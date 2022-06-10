7bc31117cb80474451e0854224ad0c89f05234b2
Martin Makarsky <martin.makarsky@fingerprintjs.com>
2022-06-10 15:26:26 +0200

    feat: add a favicon

--- pages/_app.js
+++ pages/_app.js
@@ -19,6 +19,7 @@ function App({ Component, pageProps }) {
     <ThemeProvider theme={theme}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       </Head>
 
       <Component {...pageProps} />
