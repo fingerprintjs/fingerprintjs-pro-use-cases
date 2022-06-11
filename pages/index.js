3e83732a4fe880a14661ede658ce086f2e2431f1
Martin Makarsky <martin.makarsky@fingerprintjs.com>
2022-06-11 17:18:36 +0200

    fix: admin link

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,13 @@ export default function Index() {
               backend sample implementation with a persistent data layer. The open-source repository is available at{' '}
               <a href="https://github.com/fingerprintjs/fingerprintjs-pro-use-cases">GithHub</a>.
             </p>
-            <p className='UsecaseWrapper_helper'>
-              On the <a href="/admin">admin</a> page, you can remove all info obtained from this browser. This will reenable some scenarios for
-              you if you were locked out from the specific action.
+            <p className="UsecaseWrapper_helper">
+              On the{' '}
+              <Link href="/admin">
+                <a>admin</a>
+              </Link>{' '}
+              page, you can remove all info obtained from this browser. This will reenable some scenarios for you if you
+              were locked out from the specific action.
             </p>
             <hr className="UsecaseWrapper_divider" />
             <ul>
