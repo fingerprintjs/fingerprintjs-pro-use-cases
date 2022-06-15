559630bc52151d925bca6fd8e7fc854428f33d86
Martin Makarsky <martin.makarsky@fingerprintjs.com>
2022-06-15 14:54:55 +0200

    feat: add title to the site

--- pages/_app.js
+++ pages/_app.js
@@ -20,6 +20,7 @@ function App({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <title>FingerprintJS Pro Use Cases</title>
       </Head>
 
       <Component {...pageProps} />
