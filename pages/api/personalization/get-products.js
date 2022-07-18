17042f67b26aaab988bc594cb58ee4b3b1440501
Przemysław Żydek <przemyslawzydek@gmail.com>
2022-07-18 15:58:46 +0200

    feat: use hardcoded images

--- pages/api/personalization/get-products.js
+++ pages/api/personalization/get-products.js
@@ -5,14 +5,19 @@ import { personalizationEndpoint } from '../../../api/personalization/personaliz
 
 const coffeeAdjectives = ['Smooth', 'Medium', 'Strong', 'Extra strong', 'Decaf'];
 
+const coffeeImages = [
+  'https://images.unsplash.com/photo-1519682577862-22b62b24e493?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixlib=rb-1.2.1&q=80&w=720',
+  'https://images.unsplash.com/photo-1520790564652-5e2e07fc9454?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixlib=rb-1.2.1&q=80&w=720',
+  'https://images.unsplash.com/photo-1562537238-971da66934df?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixlib=rb-1.2.1&q=80&w=720',
+  'https://images.unsplash.com/photo-1501612636467-ddc0346d843d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixlib=rb-1.2.1&q=80&w=720',
+];
+
 function getRandomArrayElement(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
 
-async function getRandomCoffeeImage() {
-  const response = await fetch('https://source.unsplash.com/random/?coffee&w=320');
-
-  return response.url;
+function getRandomCoffeeImage() {
+  return getRandomArrayElement(coffeeImages);
 }
 
 async function seedProducts() {
@@ -23,7 +28,7 @@ async function seedProducts() {
       return Product.create({
         price: Math.floor(Math.random() * 100),
         name: `${coffee} coffee`,
-        image: await getRandomCoffeeImage(),
+        image: getRandomCoffeeImage(),
         tags: [coffee],
         timestamp: new Date().getTime(),
       });
