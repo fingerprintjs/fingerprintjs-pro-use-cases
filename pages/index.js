cd81ce0573cbf55442b2ec6e68cc8226013ec450
Przemysław Żydek <przemyslawzydek@gmail.com>
2022-07-19 15:04:34 +0200

    feat: add use case tiles

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,36 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import makeStyles from '@mui/styles/makeStyles';
-import clsx from 'clsx';
 import Paper from '@mui/material/Paper';
+import { CreditScore, People, Settings } from '@mui/icons-material';
+import { PageTile } from '../components/page-tile';
+import Grid from '@mui/material/Grid';
 
-const useStyles = makeStyles((theme) => ({
-  margin: {
-    'margin-top': theme.spacing(1),
-    'margin-bottom': theme.spacing(1),
+const pages = [
+  {
+    title: 'Credential Stuffing',
+    url: '/credential-stuffing',
+    icon: <People />,
+    description: `Confirm that every visitor on your website is real and not an advanced bot using multiple techniques to create fake accounts.
+    
+You can mitigate account takeover attempts, prevent password sharing and significantly reduce the number of fake accounts.`,
   },
-}));
+  {
+    title: 'Payment Fraud',
+    url: '/payment-fraud',
+    icon: <CreditScore />,
+    description: `Identify anonymous visitors behind every transaction. Instantly recognize repeated card testing activity and link it to specific users.
+
+Significantly reduce chargebacks and fraudulent payments just one month after integrating Fingerprint on your website.`,
+  },
+  {
+    title: 'Personalization',
+    url: '/personalization',
+    icon: <Settings />,
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. .
+     
+     Integer porta mi a neque porttitor, sit amet cursus justo molestie. Vestibulum ligula ante, rhoncus ut turpis laoreet, consequat tempus odio.`,
+  },
+];
 
 export default function Index() {
   return (
@@ -31,26 +52,38 @@ export default function Index() {
               page, you can remove all info obtained from this browser. This will reenable some scenarios for you if you
               were locked out from the specific action.
             </p>
-            <hr className="UsecaseWrapper_divider" />
-            <ul>
-              <li className={clsx(useStyles().margin)}>
-                <Link href="/credential-stuffing">
-                  <a>Account Takeover, Credential Stuffing, Credential Cracking</a>
-                </Link>
-              </li>
-              <li className={clsx(useStyles().margin)}>
-                <Link href="/payment-fraud">
-                  <a>Payment Fraud, Chargeback Fraud, Card Cracking, Stolen Card</a>
-                </Link>
-              </li>
-              <li className={clsx(useStyles().margin)}>
-                <Link href="/personalization">
-                  <a>[WIP] Remember user preferences, personalization, user activity history</a>
-                </Link>
-              </li>
-            </ul>
           </div>
         </div>
+        <Grid
+          container
+          direction={{
+            xs: 'column',
+            md: 'row',
+          }}
+          spacing={3}
+          sx={{
+            marginTop: (theme) => theme.spacing(3),
+            paddingX: (theme) => theme.spacing(6),
+            marginBottom: (theme) => theme.spacing(3),
+          }}
+        >
+          {pages.map((page) => (
+            <Grid
+              key={page.url}
+              item
+              md={4}
+              xs={12}
+              sx={{
+                maxHeight: {
+                  xs: 'auto',
+                  lg: 350,
+                },
+              }}
+            >
+              <PageTile page={page} />
+            </Grid>
+          ))}
+        </Grid>
       </div>
     </Paper>
   );
