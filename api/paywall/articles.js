1d864b9bd81c0b90a7dad98e7397c9c4894ca58a
Przemysław Żydek <przemyslawzydek@gmail.com>
2022-09-16 12:56:28 +0100

    chore: use real word content instead of lorem ipsum

--- api/paywall/articles.js
+++ api/paywall/articles.js
@@ -5,29 +5,29 @@ export const articles = [
   {
     id: '1',
     date: '1 hour ago',
-    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-    content:
-      'Suspendisse sit amet ipsum eget dolor elementum tempus. Cras eget porttitor augue. Nulla facilisi. Vivamus ac fringilla risus. Nunc a blandit nibh. Etiam vel elit massa. Fusce porta neque vitae lorem porta bibendum. Sed eu dictum nisi. Ut varius interdum accumsan. Integer id velit ut mi cursus posuere. Ut vulputate ac quam et malesuada. Maecenas mollis, sapien ac auctor ultrices, lectus lorem congue risus, et consequat dui lacus a nibh. Sed rhoncus ante massa, et auctor sapien gravida vitae. Phasellus iaculis egestas dolor et tincidunt. Sed fermentum ipsum at dolor tincidunt sollicitudin.',
+    title: 'Why Browser Anti-Fingerprinting Techniques Are Not Effective',
+    content: `In this article, we will discuss why the existence of privacy-focused browsers doesn’t necessarily affect the effectiveness of fingerprint-based browser identification to prevent online fraud. 
+
+We start with a technical dive into how today’s anti-fingerprinting solutions work, focusing on uniformity and privacy-through-randomization techniques and their specific implementations. We list multiple examples from popular browsers, including one of the more popular privacy-focused browsers, Brave. We then elaborate on why device identification remains a valuable tool to prevent online fraud. Let’s dive right in.`,
   },
   {
     id: '2',
     date: '2 days ago',
-    title: 'Suspendisse sit amet ipsum eget dolor elementum tempus',
-    content:
-      'Morbi eu elit in lorem imperdiet consectetur. Suspendisse efficitur justo sed eros ullamcorper vestibulum eu ac elit. Aenean quam augue, finibus nec ultrices eget, porttitor nec neque. Praesent dapibus feugiat lectus sed interdum. Praesent mattis orci et efficitur porttitor. Cras sed elit at justo aliquet iaculis id in sapien. Fusce sit amet mattis libero, a faucibus nulla. Sed eget est tristique, vestibulum dolor non, eleifend libero. Vestibulum lobortis tempus dolor, ut iaculis quam pellentesque sit amet. Vestibulum ut elementum urna. Fusce non sodales orci, eu dignissim leo. Duis at maximus nisi.',
+    title: 'Five Mistakes You’re Making with Your Login Page Security and How to Fix Them',
+    content: `Your login page is a perfect target for hackers because it serves as the primary defense between the internet and sensitive data. Most web applications allow users to log into accounts and view sensitive information. As a result, your login page is the gateway defense between the user’s personally identifiable information (PII) and the open internet. If your login page isn’t secure, you’re likely making these five mistakes.`,
   },
   {
     id: '3',
     date: '1 week ago',
-    title: 'Morbi eu elit in lorem imperdiet consectetur',
-    content:
-      'Nulla non magna finibus, facilisis dolor eget, elementum eros. Quisque dignissim, turpis ut dignissim dictum, arcu justo vulputate nulla, nec ultricies elit felis eget tortor. Sed aliquet nibh id sapien convallis, at ornare libero cursus. Quisque vel fringilla nulla, at pellentesque arcu. Sed cursus enim sed metus iaculis feugiat. Phasellus sit amet magna vitae enim ultricies viverra. In eu eros urna. Morbi porttitor mollis sem, a dignissim neque aliquet eu. Nulla elit diam, consequat at nibh sit amet, venenatis pharetra libero. Aliquam dignissim magna ut congue posuere. Pellentesque vel felis et eros elementum tincidunt eu nec dolor. Phasellus accumsan eros id nulla facilisis lobortis.',
+    title: 'Friendly Fraud: What It Is & How to Protect Your Business',
+    content: `Friendly fraud may not sound all that threatening, but it accounts for 70% of all credit card fraud and costs the eCommerce industry billions annually.
+
+You’re doing your best to serve and relate to your customers on a human, friendly level. You’re constantly trying to make them happy, anticipate their needs, and improve their lives. You’re doing your best to provide the best customer experience, but not every customer is acting with the best intentions.`,
   },
   {
     id: '4',
     date: '1 month ago',
-    title: 'Nulla non magna finibus, facilisis dolor eget, elementum eros',
-    content:
-      'Praesent non faucibus elit. Mauris dictum nisi finibus, tempus nisi ac, maximus libero. Morbi vitae interdum turpis, ut pellentesque est. Praesent ultricies sem vel lacinia pulvinar. Praesent tincidunt accumsan dui sed dictum. Vivamus in ex est. Vivamus vel est at mauris dapibus rutrum tincidunt nec metus. Etiam vestibulum ligula vitae risus pretium, non convallis est lobortis. Curabitur quis est rhoncus urna efficitur congue. Nulla scelerisque cursus ligula egestas tempus. Maecenas ultrices a nulla id tincidunt. Suspendisse varius feugiat eros, ut pretium ante tempus ut. Duis feugiat, felis vitae dictum porta, est dolor rutrum elit, in fermentum mi nisl pretium tortor. Morbi mollis hendrerit nisl vel tincidunt. Donec sit amet sollicitudin turpis. Morbi aliquam maximus ipsum, quis tincidunt leo ultrices ut.',
+    title: 'The Basics of Loan Fraud and How To Prevent It',
+    content: `The balance between consumer convenience and security is a delicate process, especially in a competitive, high-value market. For example, when banks and credit card companies provide online loan applications, they make it convenient for potential customers to get a loan. Still, they also make it convenient for identity thieves and hackers. `,
   },
 ];
