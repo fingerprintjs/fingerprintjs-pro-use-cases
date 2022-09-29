e6a6a2821e2467efd39efa06fa10b3d33cd90255
Orkuncakilkaya <orkuncakilkaya@yandex.com.tr>
2022-09-29 21:09:19 +0300

    chore(coupon): add comments to api

--- pages/api/coupon-fraud/claim.js
+++ pages/api/coupon-fraud/claim.js
@@ -35,6 +35,7 @@ export default async function handler(req, res) {
     const isRequestIdFormatValid = /^\d{13}\.[a-zA-Z0-9]{6}$/.test(requestId);
     const isVisitorIdFormatValid = /^[a-zA-Z0-9]{20}$/.test(visitorId);
 
+    // Check if this is a valid request from a valid visitor
     if (!isRequestIdFormatValid || !isVisitorIdFormatValid) {
         return getForbiddenReponse(res, 'Invalid requestId or visitorId', 'error');
     }
@@ -44,55 +45,57 @@ export default async function handler(req, res) {
     const visitorData = await getVisitorData(visitorId, requestId);
 
     if (visitorData.error || visitorData.visits.length !== 1) {
-        // Do not allow the coupon code to be applied.
         return getForbiddenReponse(res, 'Already requested coupon, or no visitor found with visitorId', 'error');
     }
 
+
     if (new Date().getTime() - visitorData.visits[0].timestamp > 3000) {
-        // Do not allow the coupon code to be applied.
         return getForbiddenReponse(res, 'Already visited before', 'error');
     }
 
+    // Check if the visitor is trustworthy
     if (visitorData.visits[0].confidence.score < 0.95) {
-        // Do not allow the coupon code to be applied.
         return getForbiddenReponse(res, 'Visitor is not trustworthy', 'error');
     }
 
     // This is an example of obtaining the client IP address.
     // In most cases, it's a good idea to look for the right-most external IP address in the list to prevent spoofing.
     if (req.headers['x-forwarded-for'].split(',')[0] !== visitorData.visits[0].ip) {
-        // Do not allow the coupon code to be applied.
         return getForbiddenReponse(res, 'Invalid IP Address', 'error');
     }
 
     const ourOrigins = ['https://example.com',];
 
+    // Check if the visitor, made this request on a trusted origin
     const visitorDataOrigin = new URL(visitorData.visits[0].url).origin;
     if ((visitorDataOrigin !== req.headers['origin'] || !ourOrigins.includes(visitorDataOrigin) || !ourOrigins.includes(req.headers['origin']))) {
-        // Do not allow the coupon code to be applied.
+        // Remove the comment below to apply origin check
         // return getForbiddenReponse(res, 'Invalid origin', 'error');
     }
 
     const couponCode = req.body.couponCode;
     const coupon = await checkCoupon(couponCode);
 
+    // Check if the coupon exists
     if (!coupon) {
         return getForbiddenReponse(res, 'Coupon code not exists', 'error');
     }
 
     const claimed = await getVisitorClaim(visitorId, couponCode);
 
+    // Check if the visitor claimed this coupon before
     if (claimed) {
         return getForbiddenReponse(res, 'Visitor used this coupon before', 'error');
     }
 
     const recentClaim = await checkVisitorClaimedRecently(visitorId);
 
+    // Check if the visitor claimed a coupon recently
     if (recentClaim) {
         return getForbiddenReponse(res, 'Visitor claimed another coupon recently', 'error');
     }
 
-    const claim = await saveCouponCode(visitorId, couponCode);
+    await saveCouponCode(visitorId, couponCode);
 
     return getOkReponse(res, `Coupon claimed, you get 119 USD discount!`, 'success');
 }
