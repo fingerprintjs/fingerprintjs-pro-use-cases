f4ee61bf2dd2c1dc1a4a2fb8f8eb5ea63628d4ef
Orkuncakilkaya <orkuncakilkaya@yandex.com.tr>
2022-10-13 10:27:54 +0300

    refactor: reformatted with prettier

--- pages/api/admin/reset.js
+++ pages/api/admin/reset.js
@@ -16,8 +16,7 @@ import { PaymentAttempt } from '../payment-fraud/place-order';
 import { UserCartItem, UserPreferences, UserSearchHistory } from '../../../server/personalization/database';
 import { LoanRequest } from '../../../server/loan-risk/database';
 import { ArticleView } from '../../../server/paywall/database';
-import {CouponClaim} from "../coupon-fraud/claim";
-
+import { CouponClaim } from '../coupon-fraud/claim';
 
 export default async function handler(req, res) {
   // This API route accepts only POST requests.
