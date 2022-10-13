41f9a3e0e8be4f4736297ca85cb8e66a4f222607
Orkun <orkuncakilkaya@yandex.com.tr>
2022-10-13 09:42:22 +0300

    chore: remove apostrophe
    Co-authored-by: Martin Makarský <martinmakarsky@gmail.com>

--- pages/coupon-fraud/index.js
+++ pages/coupon-fraud/index.js
@@ -49,7 +49,7 @@ export default function CouponFraudUseCase() {
       listItems={[
         <>You can apply a coupon to the specific item only once.</>,
         <>You cannot spam coupon codes, there is a 1 hour threshold.</>,
-        <>You can&apos;t apply the same coupon in incognito mode</>,
+        <>You cannot apply the same coupon in incognito mode.</>,
         <>
           Sample coupon codes are <code>123456</code> and <code>098765</code>
         </>,
