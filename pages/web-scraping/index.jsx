0816c65d9bdc61f7a89b9dc47560deb6906a08f0
Juraj Uhlar <juro.uhlar@gmail.com>
2023-02-02 17:46:06 +0000

    feat: add basic search form

--- pages/web-scraping/index.jsx
+++ pages/web-scraping/index.jsx
@@ -1,6 +1,20 @@
+import { Button, FormControl, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material';
+import clsx from 'clsx';
+import { useState } from 'react';
 import { UseCaseWrapper } from '../../client/components/use-case-wrapper';
 
 export const WebScrapingUseCase = () => {
+  const [from, setFrom] = useState('');
+  const [to, setTo] = useState('');
+
+  /**
+   * @type {React.FormEventHandler<HTMLFormElement>}
+   */
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log('Submit', from, to);
+  }
+
   return (
     <>
       <UseCaseWrapper
@@ -12,13 +26,39 @@ export const WebScrapingUseCase = () => {
             In this use case, we will show how to prevent web scraping with Fingerprint Pro
         `}
         articleURL="https://fingerprintjs.com/blog/web-scraping-prevention/"
-        listItems={[
-            <>In this demo we will do something fun</>
-        ]
-        }
+        listItems={[<>In this demo we will do something fun</>]}
       >
-        <h1>Ayo</h1>
-
+        <h1>Search for flights</h1>
+        <form onSubmit={handleSubmit} className="Form_container">
+          <FormControl fullWidth>
+            <InputLabel id="from">From</InputLabel>
+            <Select labelId="from" id="from-select" value={from} label="From" onChange={(e) => setFrom(e.target.value)}>
+              <MenuItem value={'San Francisco'}>San Francisco</MenuItem>
+              <MenuItem value={'New York'}>New York</MenuItem>
+              <MenuItem value={'London'}>London</MenuItem>
+              <MenuItem value={'Tokyo'}>Tokyo</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl fullWidth>
+            <InputLabel id="to">To</InputLabel>
+            <Select labelId="to" id="to-select" value={to} label="To" onChange={(e) => setTo(e.target.value)}>
+              <MenuItem value={'San Francisco'}>San Francisco</MenuItem>
+              <MenuItem value={'New York'}>New York</MenuItem>
+              <MenuItem value={'London'}>London</MenuItem>
+              <MenuItem value={'Tokyo'}>Tokyo</MenuItem>
+            </Select>
+          </FormControl>
+          <Button
+            size="large"
+            type="submit"
+            variant="contained"
+            color="primary"
+            disableElevation
+            fullWidth
+          >
+            Search flights
+          </Button>
+        </form>
       </UseCaseWrapper>
     </>
   );
