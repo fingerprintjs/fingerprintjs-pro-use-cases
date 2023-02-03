aaf05b54eeaa90d477f9142a4672a4a25fdf9312
Przemysław Żydek <przemyslawzydek@gmail.com>
2023-02-03 16:12:19 +0100

    refactor: change type

---
+++ pages/api/identification/list-visits.js
@@ -0,0 +1,42 @@
+import { ensurePostRequest } from '../../../server/server';
+import { uniqArray } from '../../../shared/utils/array';
+import { initPresentationDemo, Visit } from '../../../server/presentation-demo/database';
+
+export default async function handler(req, res) {
+  await initPresentationDemo();
+  ensurePostRequest(req, res);
+
+  try {
+    const { visitorId, linkedId } = JSON.parse(req.body);
+
+    const rawVisit = await Visit.findOne({
+      where: {
+        visitorId,
+        linkedId: linkedId ?? null,
+      },
+    });
+
+    const visits = rawVisit ? JSON.parse(rawVisit.visits) : [];
+
+    return res.status(200).json({
+      visitorId,
+      visits,
+      ...decorateData(visits),
+    });
+  } catch (error) {
+    console.error(error);
+
+    return res.status(400).json({ message: error.message });
+  }
+}
+
+/**
+ * @param visits {import('@fingerprintjs/fingerprintjs-pro-server-api').VisitWebhook[]}
+ * */
+function decorateData(visits) {
+  return {
+    incognitoSessionsCount: visits.filter((visit) => visit.incognito).length,
+    ipAddresses: uniqArray(visits.map((visit) => visit.ip)).length,
+    locations: uniqArray(visits.map((visit) => `${visit.ipLocation.latitude}-${visit.ipLocation.longitude}`)).length,
+  };
+}
