39aee9fa9530c72eaf0263812a71a2e2dbc719d8
Przemysław Żydek <przemyslawzydek@gmail.com>
2023-02-03 14:56:50 +0100

    chore: add default api keys

--- server/fingerprint-api.js
+++ server/fingerprint-api.js
@@ -2,6 +2,8 @@ import { FingerprintJsServerApiClient } from '@fingerprintjs/fingerprintjs-pro-s
 import { resolveBackendRegion } from '../shared/region';
 
 export const fingerprintJsApiClient = new FingerprintJsServerApiClient({
-  apiKey: process.env.PRIVATE_API_KEY,
+  // In a real world use-case, we recommend using Auth-API-Key header instead: https://dev.fingerprint.com/docs/server-api#api-methods.
+  // The API key should be stored in the environment variables/secrets.
+  apiKey: process.env.PRIVATE_API_KEY ?? 'F6gQ8H8vQLc7mVsVKaFx',
   region: resolveBackendRegion(),
 });
