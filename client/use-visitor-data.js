3b78392b898e603e31b606f9e6e22a112ba00b9b
Przemysław Żydek <przemyslawzydek@gmail.com>
2023-02-09 13:20:23 +0100

    chore: set products

--- client/use-visitor-data.js
+++ client/use-visitor-data.js
@@ -21,11 +21,10 @@ async function getVisitorData({ extendedResult = true, linkedId }) {
   });
   const fp = await fpPromise;
 
-  console.log({ fp });
-
   return fp.get({
     extendedResult,
     linkedId,
+    products: ['identification'],
   });
 }
 
