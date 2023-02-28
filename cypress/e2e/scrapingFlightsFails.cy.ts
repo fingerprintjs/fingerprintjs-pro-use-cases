6397246e70faf41e35c2f5feedc659229298ccea
Juraj Uhlar <juro.uhlar@gmail.com>
2023-02-28 16:25:42 +0000

    chore: delete cypress

--- cypress/e2e/scrapingFlightsFails.cy.ts
+++
@@ -1,9 +0,0 @@
-describe('You cannot scrape flight data using cypress if Bot detection is enabled', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000/web-scraping');
-    cy.get('button:contains("Search flights")').click();
-    cy.contains('Malicious bot detected');
-  });
-});
-
-export {};
