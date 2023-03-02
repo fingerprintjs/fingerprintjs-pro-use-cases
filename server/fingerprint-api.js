0ddcee492a152d56c583ec9f30429f4289e13dfb
Juraj Uhlar <juro.uhlar@gmail.com>
2023-03-02 10:54:52 +0000

    fix: simplify SERVER_API_KEY

--- server/fingerprint-api.js
+++ server/fingerprint-api.js
@@ -1,8 +1,8 @@
 import { FingerprintJsServerApiClient } from '@fingerprintjs/fingerprintjs-pro-server-api';
 import { resolveBackendRegion } from '../shared/region';
-import {SERVER_API_KEY} from './const';
+import { SERVER_API_KEY } from './const';
 
 export const fingerprintJsApiClient = new FingerprintJsServerApiClient({
-  apiKey: process.env.PRIVATE_API_KEY ?? SERVER_API_KEY,
+  apiKey: SERVER_API_KEY,
   region: resolveBackendRegion(),
 });
