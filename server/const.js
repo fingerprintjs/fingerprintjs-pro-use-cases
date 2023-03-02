b59bfd5c0360bf06b5be5e467f8af0793944ba8a
Juraj Uhlar <juro.uhlar@gmail.com>
2023-03-02 10:53:20 +0000

    fix: Update server/const.js
    Co-authored-by: Przemysław Żydek <przemyslawzydek@gmail.com>

--- server/const.js
+++ server/const.js
@@ -6,5 +6,5 @@ export const MIN_CONFIDENCE_SCORE = 0.85;
 
 // Warning: In the real world The Server API key should be secretly stored in the environment variables/secrets.
 // We are keeping it here just to make it easy to run the demo.
-export const SERVER_API_KEY = 'F6gQ8H8vQLc7mVsVKaFx';
+export const SERVER_API_KEY = process.env.PRIVATE_API_KEY ?? 'F6gQ8H8vQLc7mVsVKaFx';
 export const PUBLIC_API_KEY = process.env.NEXT_PUBLIC_API_KEY ?? 'rzpSduhT63F6jaS35HFo';
