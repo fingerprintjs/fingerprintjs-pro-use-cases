bf16e4dea0677b4729228b0b81166cf612beadf4
Juraj Uhlar <juro.uhlar@gmail.com>
2023-05-26 17:33:15 +0100

    feat: add server api request

---
+++ pages/api/event/[requestId].ts
@@ -0,0 +1,20 @@
+import { FingerprintJsServerApiClient, Region } from '@fingerprintjs/fingerprintjs-pro-server-api';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { SERVER_API_KEY } from '../../../server/const';
+
+export default async function getFingerprintEvent(req: NextApiRequest, res: NextApiResponse) {
+  const { requestId } = req.query as { requestId: string };
+  try {
+    const client = new FingerprintJsServerApiClient({
+      region: Region.Global,
+      apiKey: SERVER_API_KEY,
+    });
+
+    const eventResponse = await client.getEvent(requestId);
+    res.status(200).json(eventResponse);
+  } catch (error) {
+    res.status(404).json({
+      message: 'requestId not found',
+    });
+  }
+}
--- pages/playground/index.tsx
+++ pages/playground/index.tsx
@@ -1,11 +1,33 @@
 import { useVisitorData } from '@fingerprintjs/fingerprintjs-pro-react';
 import { UseCaseWrapper } from '../../client/components/use-case-wrapper';
+import { useEffect, useState } from 'react';
+import { Stack } from '@mui/material';
 
 export default function Playground() {
   const { data, isLoading, getData, error } = useVisitorData(
     { extendedResult: true, ignoreCache: true },
     { immediate: true }
   );
+
+  const [eventData, setEventData] = useState<any>({});
+
+  useEffect(() => {
+    if (!data?.requestId) {
+      return;
+    }
+    console.log('JS Agent response: ', data);
+
+    fetch(`/api/event/${data.requestId}`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((eventData) => {
+        console.log('Server API Event Response: ', eventData.products);
+        setEventData(eventData);
+      });
+  }, [data]);
 
   return (
     <UseCaseWrapper
@@ -18,7 +40,10 @@ export default function Playground() {
         <button onClick={() => getData({ ignoreCache: true })}>Analyze browser again</button>
         <p>VisitorId: {isLoading ? 'Loading...' : data?.visitorId}</p>
         <p>Full visitor data:</p>
-        <pre>{error ? error.message : JSON.stringify(data, null, 2)}</pre>
+        <Stack direction={'row'} spacing={2}>
+          <pre>{error ? error.message : JSON.stringify(data, null, 2)}</pre>
+          <pre>{JSON.stringify(eventData, null, 2)}</pre>
+        </Stack>
       </div>
     </UseCaseWrapper>
   );
