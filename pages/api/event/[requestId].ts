0836c0776b66ab665671ec6abf7e4cd57066d179
Juraj Uhlar <juro.uhlar@gmail.com>
2023-05-30 09:13:41 +0100

    fix: try nodejs runtime, improve error handling

--- pages/api/event/[requestId].ts
+++ pages/api/event/[requestId].ts
@@ -2,6 +2,10 @@ import { FingerprintJsServerApiClient, Region } from '@fingerprintjs/fingerprint
 import { NextApiRequest, NextApiResponse } from 'next';
 import { SERVER_API_KEY } from '../../../server/const';
 
+export const config = {
+  runtime: 'nodejs',
+};
+
 export type IdentificationEvent = Awaited<Promise<ReturnType<typeof FingerprintJsServerApiClient.prototype.getEvent>>>;
 
 export default async function getFingerprintEvent(req: NextApiRequest, res: NextApiResponse) {
@@ -10,15 +14,20 @@ export default async function getFingerprintEvent(req: NextApiRequest, res: Next
     const client = new FingerprintJsServerApiClient({
       region: Region.Global,
       apiKey: SERVER_API_KEY,
-      fetch: fetch.bind(this),
     });
 
     const eventResponse = await client.getEvent(requestId);
     res.status(200).json(eventResponse);
   } catch (error) {
     console.log(error);
-    res.status(404).json({
-      message: 'requestId not found',
+    if (error.status === 404) {
+      res.status(404).json({
+        message: 'requestId not found',
+      });
+    }
+    res.status(500).json({
+      message: `Something went wrong ${error}`,
+      error,
     });
   }
 }
