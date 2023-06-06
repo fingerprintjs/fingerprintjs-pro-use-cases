0c9107f8b266f2aaed583aa0d0a8d8ccf7fd5a1a
Juraj Uhlar <juro.uhlar@gmail.com>
2023-06-06 15:07:05 +0200

    Map (#55)

--- client/components/playground/Map.tsx
+++ client/components/playground/Map.tsx
@@ -4,7 +4,6 @@ import { MapContainer, Marker, TileLayer } from 'react-leaflet';
 import React from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import { useUserPreferences } from '../../api/personalization/use-user-preferences';
 
 // This is a workaround for the marker icon not showing up out of the box
 let DefaultIcon = L.icon({
@@ -21,7 +20,6 @@ type MapProps = {
 };
 
 const Map: FunctionComponent<MapProps> = (props) => {
-  const { hasDarkMode } = useUserPreferences();
   return (
     <MapContainer
       center={props.position}
@@ -33,10 +31,7 @@ const Map: FunctionComponent<MapProps> = (props) => {
       doubleClickZoom={false}
       attributionControl={false}
     >
-      <TileLayer
-        // default maps: url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        url={`https://tiles.stadiamaps.com/tiles/alidade_smooth${hasDarkMode ? '_dark' : ''}/{z}/{x}/{y}{r}.png`}
-      />
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       <Marker position={props.position} icon={DefaultIcon} interactive={false} />
     </MapContainer>
   );
