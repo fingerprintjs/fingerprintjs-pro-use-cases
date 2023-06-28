9ee53fa1d18539bf9187c72520ca309d4b79ac2d
Juraj Uhlar <juro.uhlar@gmail.com>
2023-06-28 16:00:17 +0100

    chore: add placeholder (#59)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-// @ts-check
 import '../styles/globals.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ThemeProvider } from '../client/theme-provider';
@@ -53,6 +52,8 @@ function App({ Component, pageProps }) {
                 <link rel="icon" type="image/x-icon" href="/favicon.ico" />
                 <title>Fingerprint Pro Use Cases</title>
               </Head>
+              {/* Internal placeholder for deployment purposes, unrelated to any examples, please ignore */}
+              <div id="deployment-placeholder" />
               <Layout>
                 <Component {...pageProps} />
               </Layout>
