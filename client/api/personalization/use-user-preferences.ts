7df12614121af933c058d28dc8e3a783b6800b41
Juraj Uhlar <juro.uhlar@gmail.com>
2023-06-29 16:18:01 +0100

    fix: update dark more optimistically

--- client/api/personalization/use-user-preferences.ts
+++ client/api/personalization/use-user-preferences.ts
@@ -3,39 +3,41 @@ import { useVisitorData } from '../../use-visitor-data';
 import { apiRequest } from '../api';
 import { GetResult } from '@fingerprintjs/fingerprintjs-pro';
 
+type UserPreferences = {
+  hasDarkMode: boolean;
+};
+
 const GET_USER_PREFERENCES_QUERY = 'GET_USER_PREFERENCES_QUERY';
 
-function getUserPreferences(fpData: GetResult) {
+function getUserPreferences(fpData: GetResult): Promise<UserPreferences> {
   return apiRequest('/api/personalization/get-user-preferences', fpData);
 }
 
-function updateUserPreferences(fpData, hasDarkMode) {
-  return apiRequest('/api/personalization/update-user-preferences', fpData, { hasDarkMode });
+function updateUserPreferences(fpData: GetResult, preferences: UserPreferences) {
+  return apiRequest('/api/personalization/update-user-preferences', fpData, preferences);
 }
 
 export function useUserPreferences() {
-  const { data: fpData } = useVisitorData();
-
+  const { data: fingerprintResult } = useVisitorData();
   const queryClient = useQueryClient();
 
-  const userPreferencesQuery = useQuery(GET_USER_PREFERENCES_QUERY, () => getUserPreferences(fpData), {
-    enabled: Boolean(fpData),
+  const userPreferencesQuery = useQuery(GET_USER_PREFERENCES_QUERY, () => getUserPreferences(fingerprintResult), {
+    enabled: Boolean(fingerprintResult),
   });
-  const updateUserPreferencesMutation = useMutation<unknown, unknown, { hasDarkMode: boolean }>(
-    (variables) => updateUserPreferences(fpData, variables.hasDarkMode),
+
+  const updateUserPreferencesMutation = useMutation<unknown, unknown, UserPreferences>(
+    (preferences) => updateUserPreferences(fingerprintResult, preferences),
     {
-      onSuccess: async (data) => {
-        if (data) {
-          await queryClient.setQueryData(GET_USER_PREFERENCES_QUERY, data);
-        }
+      onMutate: (data) => {
+        queryClient.setQueryData(GET_USER_PREFERENCES_QUERY, data);
       },
     }
   );
 
-  const hasDarkMode = Boolean(userPreferencesQuery?.data?.data?.hasDarkMode);
+  const hasDarkMode = Boolean(userPreferencesQuery?.data?.hasDarkMode);
 
   return {
     hasDarkMode,
-    update: updateUserPreferencesMutation.mutate,
+    update: (preferences: UserPreferences) => updateUserPreferencesMutation.mutate(preferences),
   };
 }
