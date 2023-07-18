8a0274cf9e16072e09e947153fd837abbb91be33
Juraj Uhlar <juro.uhlar@gmail.com>
2023-07-18 08:45:00 +0100

    chore: exlude e2e tests from vitest config

--- vitest.config.js
+++ vitest.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, configDefaults } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
@@ -6,5 +6,6 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
+    exclude: [...configDefaults.exclude, 'e2e/**'],
   },
 });
