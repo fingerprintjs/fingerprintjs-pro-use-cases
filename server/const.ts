7ae99bf38f6f19919b995d71867231059c2d2494
Juraj Uhlar <juro.uhlar@gmail.com>
2023-08-01 12:37:56 +0100

    chore: verify confidence score env

--- server/const.ts
+++ server/const.ts
@@ -3,7 +3,9 @@ import { Region } from '@fingerprintjs/fingerprintjs-pro-server-api';
 export const IPv4_REGEX = /^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.){3}(25[0-5]|(2[0-4]|1\d|[1-9]|)\d)$/;
 export const ALLOWED_REQUEST_TIMESTAMP_DIFF_MS = 3000;
 // Confidence score thresholds might be different for different scenarios
-export const MIN_CONFIDENCE_SCORE = process.env.MIN_CONFIDENCE_SCORE ?? 0.85;
+export const MIN_CONFIDENCE_SCORE = process.env.MIN_CONFIDENCE_SCORE ? Number(process.env.MIN_CONFIDENCE_SCORE) : 0.85;
+
+console.log('MIN_CONFIDENCE_SCORE', MIN_CONFIDENCE_SCORE);
 
 const BackendRegionMap = {
   eu: Region.EU,
