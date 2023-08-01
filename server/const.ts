7ee26d7a64bb5cf4f606f5fefd7609c5ab947746
Juraj Uhlar <juro.uhlar@gmail.com>
2023-08-01 10:00:38 +0100

    feat: try lowering confidence score

--- server/const.ts
+++ server/const.ts
@@ -3,7 +3,7 @@ import { Region } from '@fingerprintjs/fingerprintjs-pro-server-api';
 export const IPv4_REGEX = /^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.){3}(25[0-5]|(2[0-4]|1\d|[1-9]|)\d)$/;
 export const ALLOWED_REQUEST_TIMESTAMP_DIFF_MS = 3000;
 // Confidence score thresholds might be different for different scenarios
-export const MIN_CONFIDENCE_SCORE = 0.85;
+export const MIN_CONFIDENCE_SCORE = 0.7;
 
 const BackendRegionMap = {
   eu: Region.EU,
