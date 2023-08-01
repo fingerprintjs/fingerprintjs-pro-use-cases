8669bb14a7ebba7c5e1904ff9029b6f289178951
Juraj Uhlar <juro.uhlar@gmail.com>
2023-08-01 14:24:17 +0100

    chore: add artificial timeout to flaky playground test

--- e2e/playground.spec.ts
+++ e2e/playground.spec.ts
@@ -15,6 +15,7 @@ const getServerResponse = async (page) => {
 const clickRefreshButton = async (page) => {
   await page.click(`[data-test="${PLAYGROUND_TAG.refreshButton}"]`);
   await page.waitForLoadState('networkidle');
+  await page.waitForTimeout(3000);
 };
 
 test.describe('Playground page page', () => {
