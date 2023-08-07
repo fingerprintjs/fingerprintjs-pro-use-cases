14a8c7ca61d57b37d6d15bd07f4d856fdbc50080
Juraj Uhlar <juro.uhlar@gmail.com>
2023-08-07 13:37:08 +0200

    feat: add learn more skeleton

--- src/client/components/common/UseCaseWrapper/UseCaseWrapper.tsx
+++ src/client/components/common/UseCaseWrapper/UseCaseWrapper.tsx
@@ -129,88 +129,10 @@ export const UseCaseWrapper: FunctionComponent<UseCaseWrapperProps> = ({
           {children}
         </Paper>
       </Container>
-      {/* <Box
-        sx={{
-          padding: (theme) => theme.spacing(4),
-          maxWidth: '600px',
-          margin: '0 auto',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <Stack direction="row" alignItems="baseline" spacing={1} justifyContent="center">
-          {returnUrl && (
-            <Link href={returnUrl} legacyBehavior passHref>
-              <Tooltip title="Go back">
-                <IconButton component="a">
-                  <ArrowBack />
-                </IconButton>
-              </Tooltip>
-            </Link>
-          )}
-          <Typography
-            variant="h1"
-            sx={{
-              textAlign: 'center',
-              pt: (t) => t.spacing(2),
-            }}
-          >
-            {title}
-          </Typography>
-        </Stack>
-        {description && (
-          <Box
-            sx={{
-              marginBottom: (t) => t.spacing(2),
-              // @ts-ignore
-              color: (t) => t.palette.gray,
-            }}
-          >
-            {description}
-          </Box>
-        )}
-        {!hideDivider && <Divider sx={{ width: '100%' }} />}
-
-        <List sx={{ lineHeight: '1rem', fontSize: '0.85rem', paddingLeft: '40px' }}>
-          {listItems?.map((item, index) => (
-            <MyListItem key={index}>{item}</MyListItem>
-          ))}
-          {showAdminLink && (
-            <MyListItem>
-              You can reset this scenario on the <Link href="/admin">Admin page</Link>.
-            </MyListItem>
-          )}
-          {articleURL && (
-            <MyListItem>
-              Learn more about this scenario in the{' '}
-              <a href={articleURL} target="_blank" rel="noreferrer">
-                {title}
-              </a>{' '}
-              article.
-            </MyListItem>
-          )}
-          {!hideSrcListItem && (
-            <MyListItem>
-              See the source code for this and other use cases{' '}
-              <a href="https://github.com/fingerprintjs/fingerprintjs-pro-use-cases" target="_blank" rel="noreferrer">
-                on Github <GitHubIcon fontSize="small" />
-              </a>
-              .
-            </MyListItem>
-          )}
-        </List>
-      </Box>
-      <Paper
-        sx={{
-          padding: (theme) => theme.spacing(4),
-          maxWidth: '600px',
-          margin: '0 auto ',
-          ...contentSx,
-        }}
-      >
-        {children}
-      </Paper> */}
+      <Container size="large" className={styles.learnMore}>
+        <h3 className={styles.learnMoreTitle}>Learn More</h3>
+        <p>Some content here</p>
+      </Container>
     </>
   );
 };
