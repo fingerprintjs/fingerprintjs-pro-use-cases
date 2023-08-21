4f39c7a4ac0d1ac1b123eb7a927a6e9a748d32df
Juraj Uhlar <juro.uhlar@gmail.com>
2023-08-21 17:26:54 +0200

    feat: refactor reset hint

--- src/client/components/common/UseCaseWrapper/UseCaseWrapper.tsx
+++ src/client/components/common/UseCaseWrapper/UseCaseWrapper.tsx
@@ -21,6 +21,22 @@ type UseCaseWrapperProps = {
   embed?: boolean;
 };
 
+type RestartHintProps = {
+  setPulseResetButton: (value: boolean) => void;
+};
+
+const RestartHint: React.FC<RestartHintProps> = ({ setPulseResetButton }) => {
+  return (
+    <b
+      onMouseEnter={() => setPulseResetButton(true)}
+      onMouseLeave={() => setPulseResetButton(false)}
+      style={{ cursor: 'help' }}
+    >
+      Restart
+    </b>
+  );
+};
+
 export const UseCaseWrapper: FunctionComponent<UseCaseWrapperProps> = ({
   children,
   hideGithubLink: hideSrcListItem = false,
