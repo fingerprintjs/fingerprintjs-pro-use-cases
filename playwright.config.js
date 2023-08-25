1a062b889538832537961363606215754c5b40ec
Juraj Uhlar <juro.uhlar@gmail.com>
2023-08-25 10:47:24 +0200

    test: try running website as part of running playwright

--- playwright.config.js
+++ playwright.config.js
@@ -87,8 +87,9 @@ module.exports = defineConfig({
   // outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
+  webServer: {
+    command: 'npm run test:start',
+    port: 3000,
+    reuseExistingServer: !process.env.CI,
+  },
 });
