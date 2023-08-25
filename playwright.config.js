ed9968e634b4715e005e597eebb4da07f92f6343
Juraj Uhlar <juro.uhlar@gmail.com>
2023-08-25 13:44:19 +0200

    chore: Delete unused code INTER-214 (#80)
    * chore: delete pages & client
    
    * chore: delete api endpoint
    
    * chore: delete server utils
    
    * chore: delete array, socket
    
    * chore: delete identification components
    
    * chore: delete Logo.tsx
    
    * chore: move zod utils
    
    * chore: remove mui styles
    
    * chore: fix playground tests
    
    * chore: remove clsx
    
    * test: try running website as part of running playwright
    
    * test: increasing timeout
    
    * test: upgrade playwright
    
    * test: go back to old way of starting web server

--- playwright.config.js
+++ playwright.config.js
@@ -88,7 +88,9 @@ module.exports = defineConfig({
 
   /* Run your local dev server before starting the tests */
   // webServer: {
-  //   command: 'npm run start',
+  //   command: 'npm run test:start',
   //   port: 3000,
+  //   reuseExistingServer: !process.env.CI,
+  //   timeout: 3 * 60 * 1000,
   // },
 });
--- src/server/socket.js
+++
@@ -1,6 +0,0 @@
-/**
- * @return import('socket.io').Socket
- * */
-export function extractSocketServer(req) {
-  return req?.socket?.server?.io;
-}
