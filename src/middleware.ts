71cf4d3d612e2c5a037335d33b0aaedda0ad1e09
Juraj Uhlar <juro.uhlar@gmail.com>
2023-09-08 10:33:58 +0200

    remove port, set protocol to https

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,15 +5,14 @@ import type { NextRequest } from 'next/server';
  * This middleware runs on every requests and redirects from fingerprinthub.com/* to demo.fingerprint.com/*
  */
 export function middleware(request: NextRequest) {
-  // Note: We need to read the hostname from a header, request.url, request.nextURL just say "localhost"
+  // Note: We need to read the hostname from a header, request.url, request.nextURL just say "localhost" on Digital
   const host = request.headers.get('host');
   // To-do: Change to fingerprinthub.com when the switch happens
   if (host === 'staging.fingerprinthub.com') {
     const newURL = request.nextUrl.clone();
     newURL.host = 'demo.fingerprint.com';
-    // Uncomment for testing on localhost
-    // newURL.protocol = 'https';
-    // newURL.port = '';
+    newURL.protocol = 'https';
+    newURL.port = '';
     return NextResponse.redirect(newURL);
   }
 }
