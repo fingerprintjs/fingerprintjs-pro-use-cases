4f06999a4c9b9547f1b0b63530a510da7fc10230
Juraj Uhlar <juro.uhlar@gmail.com>
2023-09-08 10:13:06 +0200

    use headers host

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,8 +5,10 @@ import type { NextRequest } from 'next/server';
  * This middleware runs on every requests and redirects from fingerprinthub.com/* to demo.fingerprint.com/*
  */
 export function middleware(request: NextRequest) {
-  // To-do: Change to fingerprinthub.com
-  if (request.nextUrl.hostname === 'staging.fingerprinthub.com') {
+  // Note: We need to read the hostname from a header, request.url, request.nextURL just say "localhost"
+  const host = request.headers.get('host');
+  // To-do: Change to fingerprinthub.com when the switch happens
+  if (host === 'staging.fingerprinthub.com') {
     const newURL = request.nextUrl.clone();
     newURL.host = 'demo.fingerprint.com';
     // Uncomment for testing on localhost
