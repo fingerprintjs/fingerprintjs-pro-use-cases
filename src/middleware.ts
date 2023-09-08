9c34adf8df6bac6030acdeed0d24d37c8efdcbc2
Juraj Uhlar <juro.uhlar@gmail.com>
2023-09-08 10:39:28 +0200

    chore: refactor

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,14 +5,12 @@ import type { NextRequest } from 'next/server';
  * This middleware runs on every requests and redirects from fingerprinthub.com/* to demo.fingerprint.com/*
  */
 export function middleware(request: NextRequest) {
-  // Note: We need to read the hostname from a header, request.url, request.nextURL just say "localhost" on Digital
+  // Note: We need to read the hostname from a header because request.url, request.nextURL just say "localhost" on Digital Ocean
   const host = request.headers.get('host');
   // To-do: Change to fingerprinthub.com when the switch happens
   if (host === 'staging.fingerprinthub.com') {
-    const newURL = request.nextUrl.clone();
-    newURL.host = 'demo.fingerprint.com';
-    newURL.protocol = 'https';
-    newURL.port = '';
+    const newURL = new URL(request.nextUrl.pathname, 'https://demo.fingerprint.com');
+    newURL.search = request.nextUrl.search;
     return NextResponse.redirect(newURL);
   }
 }
