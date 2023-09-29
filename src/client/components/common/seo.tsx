e9fa9a550c01bb24a242c10ed88cde2ab312dd69
Juraj Uhlar <juro.uhlar@gmail.com>
2023-09-29 16:34:14 +0200

    chore: fix meta url

--- src/client/components/common/seo.tsx
+++ src/client/components/common/seo.tsx
@@ -6,10 +6,12 @@ type HeadSEOProps = {
   title: string;
   description: string;
   image?: string;
+  url?: string;
 };
 
-export const SEO: FunctionComponent<HeadSEOProps> = ({ title, description, image }) => {
-  const metaImage = image ?? `/fingerprintDefaultMetaImage.png`;
+export const SEO: FunctionComponent<HeadSEOProps> = ({ title, description, image, url }) => {
+  const metaImage = image ?? `${PRODUCTION_URL}/fingerprintDefaultMetaImage.png`;
+  const metaUrl = url ?? PRODUCTION_URL;
   return (
     <Head>
       <title>{title}</title>
@@ -21,11 +23,13 @@ export const SEO: FunctionComponent<HeadSEOProps> = ({ title, description, image
       <meta property="og:description" content={description} />
       <meta property="og:site_name" content="Fingerprint Use Cases" />
       <meta property="og:image" content={metaImage} />
+      <meta property="og:url" content={metaUrl} />
 
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content={metaImage} />
+      <meta name="twitter:url" content={metaUrl} />
     </Head>
   );
 };
