10ae9cb80ee1f016ce06b633ec3f4718cc67b862
Juraj Uhlar <juro.uhlar@gmail.com>
2023-09-29 10:47:39 +0200

    chore: redirect from fingerprinthub.com to demo.fingerprint.com

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import type { NextRequest } from 'next/server';
+
+/**
+ * This middleware runs on every requests and redirects from fingerprinthub.com/* to demo.fingerprint.com/*
+ */
+export function middleware(request: NextRequest) {
+  // Note: We need to read the hostname from a header because request.url, request.nextURL just say "localhost" on Digital Ocean
+  const host = request.headers.get('host');
+  if (host === 'fingerprinthub.com') {
+    const newURL = new URL(request.nextUrl.pathname, 'https://demo.fingerprint.com');
+    newURL.search = request.nextUrl.search;
+    return Response.redirect(newURL, 301);
+  }
+}
