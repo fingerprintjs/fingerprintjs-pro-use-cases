c82f590dd41de3d7478b34da3e352d7f2a19df24
Juraj Uhlar <juro.uhlar@gmail.com>
2023-09-29 15:35:13 +0200

    feat: add sitemap

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,39 @@
+import { HOMEPAGE_CARDS } from '../client/components/common/content';
+
+const PRODUCTION_HOST = 'https://demo.fingerprint.com';
+
+function generateSiteMap() {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <url>
+       <loc>${PRODUCTION_HOST}</loc>
+     </url>
+     ${HOMEPAGE_CARDS.map(({ url }) => {
+       return `
+       <url>
+           <loc>${`${PRODUCTION_HOST}${url}`}</loc>
+       </url>
+     `;
+     }).join('')}
+   </urlset>
+ `;
+}
+
+function SiteMap() {
+  // getServerSideProps will do the heavy lifting
+}
+
+export async function getServerSideProps({ res }) {
+  const sitemap = generateSiteMap();
+
+  res.setHeader('Content-Type', 'text/xml');
+  // we send the XML to the browser
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default SiteMap;
