46327cc0f4113b7a56524112411701d073d61426
Juraj Uhlar <juro.uhlar@gmail.com>
2023-10-30 21:03:05 +0000

    styles: fix svgs with currentColor

--- src/client/img/buttonMinusSvg.tsx
+++ src/client/img/buttonMinusSvg.tsx
@@ -1,7 +1,15 @@
 import { ComponentProps, FunctionComponent } from 'react';
 
 export const ButtonMinusSvg: FunctionComponent<ComponentProps<'svg'>> = (props) => (
-  <svg width="15" height="16" viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+  <svg
+    width="15"
+    height="16"
+    viewBox="0 0 15 16"
+    fill="currentColor"
+    stroke="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
     <path d="M10.7932 7.66016H7.2501H3.70703" strokeWidth="0.9" strokeLinecap="round" strokeLinejoin="round" />
   </svg>
 );
--- src/client/img/buttonPlusSvg.tsx
+++ src/client/img/buttonPlusSvg.tsx
@@ -1,15 +1,20 @@
 import { FunctionComponent } from 'react';
 
 export const ButtonPlusSvg: FunctionComponent<React.ComponentProps<'svg'>> = (props) => (
-  <svg width="15" height="16" viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <svg width="14" height="16" viewBox="0 0 14 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M7.08408 3.81836V7.66014M7.08408 7.66014V11.5019M7.08408 7.66014H10.6271M7.08408 7.66014H3.54102"
-        strokeWidth="0.9"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-    ;
+  <svg
+    width="15"
+    height="16"
+    viewBox="0 0 15 16"
+    fill="currentColor"
+    stroke="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M7.08408 3.81836V7.66014M7.08408 7.66014V11.5019M7.08408 7.66014H10.6271M7.08408 7.66014H3.54102"
+      strokeWidth="0.9"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
   </svg>
 );
