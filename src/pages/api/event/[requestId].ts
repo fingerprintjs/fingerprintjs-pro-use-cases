7b5d4784e0c228d4a2881b2da24648cb032c4cf8
Martin Makarský <martin.makarsky@fingerprintjs.com>
2023-11-17 16:40:45 +0100

    feat: add retry logic for 404 /events Server API requests INTER-402 (#101)
    * feat: add retry logic for 404 /events Server API requests INTER-402
    
    * fix: use the existing Server API client INTER-402
    
    * Update src/pages/api/event/[requestId].ts
    
    Co-authored-by: Juraj Uhlar <juro.uhlar@gmail.com>
    
    * fix: simplify retry logic for the /events requests INTER-402
    
    ---------
    
    Co-authored-by: Juraj Uhlar <juro.uhlar@gmail.com>

--- src/pages/api/event/[requestId].ts
+++ src/pages/api/event/[requestId].ts
@@ -1,28 +1,44 @@
-import { FingerprintJsServerApiClient, isEventError } from '@fingerprintjs/fingerprintjs-pro-server-api';
+import { isEventError } from '@fingerprintjs/fingerprintjs-pro-server-api';
 import { NextApiRequest, NextApiResponse } from 'next';
-import { BACKEND_REGION, SERVER_API_KEY } from '../../../server/const';
+import { fingerprintJsApiClient } from '../../../server/fingerprint-api';
 
 export default async function getFingerprintEvent(req: NextApiRequest, res: NextApiResponse) {
   const { requestId } = req.query as { requestId: string };
-  const client = new FingerprintJsServerApiClient({
-    region: BACKEND_REGION,
-    apiKey: SERVER_API_KEY,
-  });
+  return await tryGetFingerprintEvent(res, requestId);
+}
 
+/**
+ * It's possible the data about the specific event is not propagated to the Server API yet. This function retries for Not Found (404) requests.
+ * */
+async function tryGetFingerprintEvent(
+  res: NextApiResponse,
+  requestId: string,
+  retryCount = 5,
+  retryDelay: number = 3000,
+) {
   try {
-    const eventResponse = await client.getEvent(requestId);
+    const eventResponse = await fingerprintJsApiClient.getEvent(requestId);
     res.status(200).json(eventResponse);
   } catch (error) {
-    console.log(error);
-    if (isEventError(error)) {
-      res.statusMessage = `${error.error.code} - ${error.error.message}`;
-      res.status(error.status).json({
-        message: error.error.message,
-        code: error.error.code,
-      });
+    // Retry only Not Found (404) requests.
+    if (error.status === 404 && retryCount > 1) {
+      setTimeout(() => tryGetFingerprintEvent(res, requestId, retryCount - 1, retryDelay), retryDelay);
     } else {
-      res.statusMessage = `Something went wrong ${error}`;
-      res.status(500);
+      console.error(error);
+      sendErrorResponse(res, error);
     }
+  }
+}
+
+function sendErrorResponse(res: NextApiResponse, error: any) {
+  if (isEventError(error)) {
+    res.statusMessage = `${error.error.code} - ${error.error.message}`;
+    res.status(error.status).json({
+      message: error.error.message,
+      code: error.error.code,
+    });
+  } else {
+    res.statusMessage = `Something went wrong ${error}`;
+    res.status(500).end();
   }
 }
