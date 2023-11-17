55fa91ddd678d87e053ea8e83a1e45afdac974b1
Martin Makarsky <martin.makarsky@fingerprintjs.com>
2023-11-17 13:43:27 +0100

    fix: simplify retry logic for the /events requests INTER-402

--- src/pages/api/event/[requestId].ts
+++ src/pages/api/event/[requestId].ts
@@ -4,7 +4,6 @@ import { fingerprintJsApiClient } from '../../../server/fingerprint-api';
 
 export default async function getFingerprintEvent(req: NextApiRequest, res: NextApiResponse) {
   const { requestId } = req.query as { requestId: string };
-
   return await tryGetFingerprintEvent(res, requestId);
 }
 
@@ -21,23 +20,17 @@ async function tryGetFingerprintEvent(
     const eventResponse = await fingerprintJsApiClient.getEvent(requestId);
     res.status(200).json(eventResponse);
   } catch (error) {
-    console.error(error);
-
-    if (retryCount === 1) {
-      handleFinalRetry(res, error);
-      return;
-    }
-
     // Retry only Not Found (404) requests.
-    if (error.status === 404) {
+    if (error.status === 404 && retryCount > 1) {
       setTimeout(() => tryGetFingerprintEvent(res, requestId, retryCount - 1, retryDelay), retryDelay);
     } else {
-      handleOtherError(res, error);
+      console.error(error);
+      sendErrorResponse(res, error);
     }
   }
 }
 
-function handleFinalRetry(res: NextApiResponse, error: any) {
+function sendErrorResponse(res: NextApiResponse, error: any) {
   if (isEventError(error)) {
     res.statusMessage = `${error.error.code} - ${error.error.message}`;
     res.status(error.status).json({
@@ -45,11 +38,7 @@ function handleFinalRetry(res: NextApiResponse, error: any) {
       code: error.error.code,
     });
   } else {
-    handleOtherError(res, error);
+    res.statusMessage = `Something went wrong ${error}`;
+    res.status(500).end();
   }
-}
-
-function handleOtherError(res: NextApiResponse, error: any) {
-  res.statusMessage = `Something went wrong ${error}`;
-  res.status(500).end();
 }
