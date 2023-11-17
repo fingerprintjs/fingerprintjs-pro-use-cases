29e5626b18216196489267eae77038e812057718
Martin Makarsky <martin.makarsky@fingerprintjs.com>
2023-11-17 12:24:46 +0100

    feat: add retry logic for 404 /events Server API requests INTER-402

--- src/pages/api/event/[requestId].ts
+++ src/pages/api/event/[requestId].ts
@@ -9,20 +9,52 @@ export default async function getFingerprintEvent(req: NextApiRequest, res: Next
     apiKey: SERVER_API_KEY,
   });
 
+  return await tryGetFingerprintEvent(res, client, requestId);
+}
+
+/**
+ * It's possible the data about the specific event is not propagated to the Server API yet. This function retries for Not Found (404) requests.
+ * */
+async function tryGetFingerprintEvent(
+  res: NextApiResponse,
+  client: FingerprintJsServerApiClient,
+  requestId: string,
+  retryCount = 5,
+  retryDelay: number = 3000,
+) {
   try {
     const eventResponse = await client.getEvent(requestId);
     res.status(200).json(eventResponse);
   } catch (error) {
-    console.log(error);
-    if (isEventError(error)) {
-      res.statusMessage = `${error.error.code} - ${error.error.message}`;
-      res.status(error.status).json({
-        message: error.error.message,
-        code: error.error.code,
-      });
+    console.error(error);
+
+    if (retryCount === 1) {
+      handleFinalRetry(res, error);
+      return;
+    }
+
+    // Retry only Not Found (404) requests.
+    if (error.status === 404) {
+      setTimeout(() => tryGetFingerprintEvent(res, client, requestId, retryCount - 1, retryDelay), retryDelay);
     } else {
-      res.statusMessage = `Something went wrong ${error}`;
-      res.status(500);
+      handleOtherError(res, error);
     }
   }
+}
+
+function handleFinalRetry(res: NextApiResponse, error: any) {
+  if (isEventError(error)) {
+    res.statusMessage = `${error.error.code} - ${error.error.message}`;
+    res.status(error.status).json({
+      message: error.error.message,
+      code: error.error.code,
+    });
+  } else {
+    handleOtherError(res, error);
+  }
+}
+
+function handleOtherError(res: NextApiResponse, error: any) {
+  res.statusMessage = `Something went wrong ${error}`;
+  res.status(500);
 }
