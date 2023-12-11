c8703c9cae0b9977e3deefae8a77a70581a4c49b
Juraj Uhlar <juro.uhlar@gmail.com>
2023-12-11 12:28:51 +0000

    chore: review fixes

--- playwright.config.js
+++ playwright.config.js
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 /**
  * Read environment variables from file.
- * https://github.com/motdotla/dotenv;
+ * https://github.com/motdotla/dotenv
  */
 import 'dotenv/config';
 
