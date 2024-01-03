e53107c1ec9651760b9c4bba968c5a1232f1daed
Juraj Uhlar <juro.uhlar@gmail.com>
2024-01-03 15:55:32 +0100

    test: try removing network idle to fix flakiness

--- e2e/bot-firewall.spec.ts
+++ e2e/bot-firewall.spec.ts
@@ -11,29 +11,29 @@ test.describe('Bot Firewall Demo', () => {
 
   test('Should display bot visit a allow blocking IP address', async ({ page }) => {
     // Record bot visit in web-scraping page
-    await page.goto('/web-scraping', { waitUntil: 'networkidle' });
+    await page.goto('/web-scraping');
     await expect(page.getByTestId(TEST_IDS.common.alert)).toContainText('Malicious bot detected');
 
     // Check bot visit record and block IP
-    await page.goto('/bot-firewall', { waitUntil: 'networkidle' });
+    await page.goto('/bot-firewall');
     await page.getByRole('button', { name: BOT_FIREWALL_COPY.blockIp }).first().click();
     await page.getByText('was blocked in the application firewall').waitFor();
     await page.waitForTimeout(5000);
 
     // Try to visit web-scraping page, should be blocked by Cloudflare
-    await page.goto('https://staging.fingerprinthub.com/web-scraping', { waitUntil: 'networkidle' });
-    await page.reload({ waitUntil: 'networkidle' });
+    await page.goto('https://staging.fingerprinthub.com/web-scraping');
+    await page.reload();
     await page.getByRole('heading', { name: 'Sorry, you have been blocked' }).waitFor();
 
     // Unblock IP
-    await page.goto('/bot-firewall', { waitUntil: 'networkidle' });
+    await page.goto('/bot-firewall');
     await page.getByRole('button', { name: BOT_FIREWALL_COPY.unblockIp }).first().click();
     await page.getByText('was unblocked in the application firewall').waitFor();
     await page.waitForTimeout(5000);
 
     // Try to visit web-scraping page, should be allowed again
-    await page.goto('https://staging.fingerprinthub.com/web-scraping', { waitUntil: 'networkidle' });
-    await page.reload({ waitUntil: 'networkidle' });
+    await page.goto('https://staging.fingerprinthub.com/web-scraping');
+    await page.reload();
     await expect(page.getByTestId(TEST_IDS.common.alert)).toContainText('Malicious bot detected');
   });
 });
