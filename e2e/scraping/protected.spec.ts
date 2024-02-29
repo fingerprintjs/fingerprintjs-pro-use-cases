72b1e5084d230c8ba2035cb30d7ee8e6515aba26
Juraj Uhlar <juro.uhlar@gmail.com>
2024-02-29 10:10:50 -0300

    chore: fix flaky botd test

--- e2e/scraping/protected.spec.ts
+++ e2e/scraping/protected.spec.ts
@@ -4,6 +4,8 @@ import { TEST_IDS } from '../../src/client/testIDs';
 test.describe('Scraping flights', () => {
   test('is not possible with Bot detection on', async ({ page }) => {
     await page.goto('/web-scraping', { waitUntil: 'networkidle' });
-    await expect(page.getByTestId(TEST_IDS.common.alert)).toContainText('Malicious bot detected');
+    await expect(page.getByTestId(TEST_IDS.common.alert)).toContainText('Malicious bot detected', {
+      timeout: 10000,
+    });
   });
 });
