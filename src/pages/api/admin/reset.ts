d4633b2ce860177f308c3416c307ac7cf599302e
Juraj Uhlar <juro.uhlar@gmail.com>
2024-03-25 18:00:16 +0000

    feat: add db model to reset

--- src/pages/api/admin/reset.ts
+++ src/pages/api/admin/reset.ts
@@ -13,6 +13,7 @@ import { getAndValidateFingerprintResult } from '../../../server/checks';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { deleteBlockedIp } from '../../../server/botd-firewall/blockedIpsDatabase';
 import { syncFirewallRuleset } from '../../../server/botd-firewall/cloudflareApiHelper';
+import { SmsVerificationModel } from '../../../server/sms-fraud/database';
 
 export type ResetResponse = {
   message: string;
@@ -78,6 +79,7 @@ const deleteVisitorData = async (visitorId: string, ip: string) => {
       await syncFirewallRuleset();
       return deletedIpCount;
     }),
+    deletedSmsVerificationRequests: await tryToDestroy(() => SmsVerificationModel.destroy(options)),
   };
 };
 
