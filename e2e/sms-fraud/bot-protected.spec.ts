790b8f968093df1c75fd07c354f53f16921fdc22
Juraj Uhlar <juro.uhlar@gmail.com>
2024-04-03 23:18:13 +0200

    test: refactor bot protected

--- e2e/sms-fraud/bot-protected.spec.ts
+++ e2e/sms-fraud/bot-protected.spec.ts
@@ -1,5 +1,6 @@
-import { expect, test } from '@playwright/test';
-import { TEST_ATTRIBUTES, TEST_IDS } from '../../src/client/testIDs';
+import { test } from '@playwright/test';
+import { TEST_IDS } from '../../src/client/testIDs';
+import { assertAlert } from '../e2eTestUtils';
 
 // TODO Remove once fixed
 test.skip(({ browserName }) => browserName == 'firefox', 'This test currently fails in Github CI on Firefox');
@@ -8,8 +9,6 @@ test.describe('Sending verification SMS messages', () => {
   test('is not possible as a bot with Bot detection on', async ({ page }) => {
     await page.goto('/sms-fraud');
     await page.getByTestId(TEST_IDS.smsFraud.sendMessage).click();
-    const alert = await page.getByTestId(TEST_IDS.common.alert);
-    await expect(alert).toHaveAttribute(TEST_ATTRIBUTES.severity, 'error');
-    await expect(alert).toContainText('Malicious bot detected');
+    assertAlert({ page, severity: 'error', text: 'Malicious bot detected' });
   });
 });
