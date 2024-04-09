f7e17c2656741b5554308994bf336884334113de
Juraj Uhlar <juro.uhlar@gmail.com>
2024-04-09 21:59:00 +0200

    feat: return success

--- src/app/location-spoofing/api/activate-ppp/route.ts
+++ src/app/location-spoofing/api/activate-ppp/route.ts
@@ -1,3 +1,4 @@
+import { NextResponse } from 'next/server';
 import { Severity, getAndValidateFingerprintResult } from '../../../../server/checks';
 
 export type ActivateRegionalPricingPayload = {
@@ -8,15 +9,59 @@ export type ActivateRegionalPricingPayload = {
 export type ActivateRegionalPricingResponse = {
   severity: Severity;
   message: string;
-  data: {
+  data?: {
     discount: number;
   };
 };
 
-export async function POST(request: Request) {
+export async function POST(request: Request): Promise<NextResponse<ActivateRegionalPricingResponse>> {
   const { requestId } = (await request.json()) as ActivateRegionalPricingPayload;
 
+  // Get the full Identification result from Fingerprint Server API and validate its authenticity
   const fingerprintResult = await getAndValidateFingerprintResult(requestId, request);
+  if (!fingerprintResult.okay) {
+    return NextResponse.json({ severity: 'error', message: fingerprintResult.error }, { status: 403 });
+  }
 
-  return Response.json({ fingerprintResult });
+  const ipInfo = fingerprintResult.data.products?.ipInfo?.data;
+  const country = ipInfo?.v6?.geolocation?.country ?? ipInfo?.v4?.geolocation?.country;
+  const vpnDetection = fingerprintResult.data.products?.vpn?.data;
+
+  if (!country) {
+    return NextResponse.json(
+      {
+        severity: 'error',
+        message: 'Location could not be determined, please try a different browser or internet connection.',
+      },
+      { status: 403 },
+    );
+  }
+
+  if (vpnDetection?.result === true) {
+    let reason = '';
+    if (vpnDetection.methods?.publicVPN) {
+      reason = `Your IP address appears to be in ${country.name} but it's a known VPN IP address.`;
+    }
+    if (vpnDetection.methods?.timezoneMismatch && vpnDetection.originTimezone) {
+      reason = `Your IP address appears to be in ${country.name}, but your timezone is ${vpnDetection.originTimezone}.`;
+    }
+    if (vpnDetection.methods?.auxiliaryMobile) {
+      reason = `Your IP address appears to be in ${country.name}, but your phone is not.`;
+    }
+    return NextResponse.json(
+      {
+        severity: 'error',
+        message: `It seems you are using a VPN. Please turn it off before activating regional pricing. ${reason}`,
+      },
+      { status: 403 },
+    );
+  }
+
+  const discount = 45;
+
+  return NextResponse.json({
+    severity: 'success',
+    message: `Yay! We have applied a regional discount of ${discount}%. With this discount your purchase will be restricted to ${country.name}.`,
+    data: { discount: 45 },
+  });
 }
