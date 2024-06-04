2d4686e48f429efb3988c217ac02d37eacd99421
Juraj Uhlar <juro.uhlar@gmail.com>
2024-06-04 17:00:00 +0100

    feat: nicer map

--- src/app/playground/components/Map.tsx
+++ src/app/playground/components/Map.tsx
@@ -8,10 +8,15 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 // This is a workaround for the marker icon not showing up out of the box
-const DefaultIcon = L.icon({
-  iconUrl: '/marker-icon.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
+const DefaultIcon = L.divIcon({
+  html: `<svg width="30" height="30" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 30 30">
+          <circle cx="15" cy="15" r="15" fill="#FF5D22" opacity="0.5"/>
+          <circle cx="15" cy="15" r="7" fill="#FF5D22"/>
+        </svg>`,
+  iconSize: [30, 30],
+  iconAnchor: [15, 15],
+  // Necessary to prevent default border and background of .leaflet-div-icon
+  className: 'playground-map-marker',
 });
 L.Marker.prototype.options.icon = DefaultIcon;
 
@@ -33,7 +38,9 @@ const Map: FunctionComponent<MapProps> = (props) => {
       doubleClickZoom={false}
       attributionControl={false}
     >
-      <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+      {/* More options here https://github.com/leaflet-extras/leaflet-providers */}
+      {/* <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' /> */}
+      <TileLayer url='https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png' />
       <Marker position={props.position} icon={DefaultIcon} interactive={false} />
     </MapContainer>
   );
