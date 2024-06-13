98589ec7c0762866e259ec9e3d89445caaf594e0
Juraj Uhlar <juro.uhlar@gmail.com>
2024-06-13 13:41:21 +0100

    feat: improve scrollbar position

--- src/client/components/common/CodeSnippet/CodeSnippet.tsx
+++ src/client/components/common/CodeSnippet/CodeSnippet.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren } from 'react';
+import { CSSProperties, PropsWithChildren } from 'react';
 import { PrismAsyncLight } from 'react-syntax-highlighter';
 import lightTheme from 'react-syntax-highlighter/dist/cjs/styles/prism/coy';
 import darkTheme from 'react-syntax-highlighter/dist/cjs/styles/prism/coldark-dark';
@@ -28,32 +28,42 @@ export function CodeSnippet({
 }: PropsWithChildren<CodeSnippetProps>) {
   const hasDarkMode = false;
 
-  const PRISM_CUSTOM_STYLE = {
-    backgroundColor: '#FFF',
-    padding: '16px 12px',
-    borderRadius: '8px',
-    fontFamily: 'Roboto Mono, monospace',
-    fontSize: '14px',
-    margin: '0',
+  const PRISM_CUSTOM_STYLE: CSSProperties = {
+    padding: '16px',
+    fontFamily: 'JetBrains Mono, monospace',
+    fontSize: '16px',
     overflow: 'auto',
-    border: '1px solid #D0D0D0',
+    marginTop: '0',
+    marginBottom: '0',
+    marginLeft: '0',
+    marginRight: '0',
+    // lineHeight: '205%',
     height: '446px',
   };
   const PRISM_CODE_TAG_PROPS = { style: { color: '#c92c2c', font: 'inherit' as const } };
 
   return (
-    <PrismAsyncLight
-      showLineNumbers={showLineNumbers}
-      lineNumberStyle={PRISM_LINE_NUMBER_STYLE}
-      wrapLines
-      language={language}
-      style={hasDarkMode ? darkTheme : lightTheme}
-      customStyle={PRISM_CUSTOM_STYLE}
-      codeTagProps={PRISM_CODE_TAG_PROPS}
-      className={classnames(styles.snippet, className)}
-      data-testid={dataTestId}
+    <div
+      style={{
+        padding: '3px',
+        backgroundColor: '#FFF',
+        borderRadius: '8px',
+        border: '1px solid #D0D0D0',
+      }}
     >
-      {children}
-    </PrismAsyncLight>
+      <PrismAsyncLight
+        showLineNumbers={showLineNumbers}
+        lineNumberStyle={PRISM_LINE_NUMBER_STYLE}
+        wrapLines
+        language={language}
+        style={hasDarkMode ? darkTheme : lightTheme}
+        customStyle={PRISM_CUSTOM_STYLE}
+        codeTagProps={PRISM_CODE_TAG_PROPS}
+        className={classnames(styles.snippet, className)}
+        data-testid={dataTestId}
+      >
+        {children}
+      </PrismAsyncLight>
+    </div>
   );
 }
