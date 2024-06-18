9c585c6718056477d3c2c3ecffd94751255b17b9
Juraj Uhlar <juro.uhlar@gmail.com>
2024-06-18 12:38:51 +0100

    try react18-json-viewer, much better

--- src/client/components/common/CodeSnippet/CodeSnippet.tsx
+++ src/client/components/common/CodeSnippet/CodeSnippet.tsx
@@ -7,7 +7,8 @@ import styles from './CodeSnippet.module.scss';
 import classnames from 'classnames';
 import { MyScrollArea } from '../ScrollArea/ScrollArea';
 import { MyCopyButton } from '../CopyButton/CopyButton';
-import ReactJsonViewer from '@microlink/react-json-view';
+import JsonView from 'react18-json-view';
+import 'react18-json-view/src/style.css';
 
 export interface CodeSnippetProps {
   language: string;
@@ -44,31 +45,6 @@ export function CodeSnippet({
   };
   const PRISM_CODE_TAG_PROPS = { style: { color: '#c92c2c', font: 'inherit' as const } };
 
-  const theme = {
-    keys: 'rgb(201, 44, 44)', // red
-    stringValue: 'rgb(47, 156, 10)', // green
-    // number, boolean
-    codeValue: 'rgb(201, 44, 44)', // red
-    brackets: 'rgb(95, 99, 100)', // dark gray
-  };
-
-  //   base00 - Default Background
-  // base01 - Lighter Background (Used for status bars, line number and folding marks)
-  // base02 - Selection Background
-  // base03 - Comments, Invisibles, Line Highlighting
-  // base04 - Dark Foreground (Used for status bars)
-  // base05 - Default Foreground, Caret, Delimiters, Operators
-  // base06 - Light Foreground (Not often used)
-  // base07 - Light Background (Not often used)
-  // base08 - Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
-  // base09 - Integers, Boolean, Constants, XML Attributes, Markup Link Url
-  // base0A - Classes, Markup Bold, Search Text Background
-  // base0B - Strings, Inherited Class, Markup Code, Diff Inserted
-  // base0C - Support, Regular Expressions, Escape Characters, Markup Quotes
-  // base0D - Functions, Methods, Attribute IDs, Headings
-  // base0E - Keywords, Storage, Selector, Markup Italic, Diff Changed
-  // base0F - Deprecated, Opening/Closing Embedded Language Tags, e.g. <?php ?>
-
   return (
     <div className={styles.snippetContainer}>
       <div className={styles.copyButtonContainer}>
@@ -77,36 +53,7 @@ export function CodeSnippet({
       <MyScrollArea className={styles.scrollArea}>
         {collapsibleJSON ? (
           <div className={styles.reactJsonViewerWrapper}>
-            <ReactJsonViewer
-              src={children as Object}
-              // theme='bright:inverted'
-              theme={{
-                base00: '#fff', // Default Background
-                base01: '#fff', // Lighter Background (Used for status bars, line number and folding marks)
-                base02: theme.brackets, // Selection Background
-                base03: theme.brackets, // Comments, Invisibles, Line Highlighting
-                base04: theme.brackets, // Dark Foreground (Used for status bars)
-                base05: theme.brackets, // Default Foreground, Caret, Delimiters, Operators
-                base06: '', // Light Foreground (Not often used)
-                base07: theme.codeValue, // Light Background (Not often used)
-                base08: '', // Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
-                base09: theme.stringValue, // Integers, Boolean, Constants, XML Attributes, Markup Link Url
-                base0A: theme.codeValue, // Classes, Markup Bold, Search Text Background
-                base0B: theme.codeValue, // Strings, Inherited Class, Markup Code, Diff Inserted
-                base0C: theme.codeValue, // Array indexes in JSON, Support, Regular Expressions, Escape Characters, Markup Quotes
-                base0D: theme.codeValue, // Functions, Methods, Attribute IDs, Headings
-                base0E: theme.codeValue, // Keywords, Storage, Selector, Markup Italic, Diff Changed
-                base0F: theme.codeValue, // Deprecated, Opening/Closing Embedded Language Tags, e.g. <?php ?>
-              }}
-              name={false}
-              enableClipboard={false}
-              displayDataTypes={false}
-              displayObjectSize={false}
-              collapseStringsAfterLength={40}
-              collapsed={2}
-              iconStyle='square'
-              indentWidth={2}
-            />
+            <JsonView src={children} ignoreLargeArray />
           </div>
         ) : (
           <PrismAsyncLight
