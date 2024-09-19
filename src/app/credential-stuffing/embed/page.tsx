c2dd3ff6bc15bea1899e0434521d7e50fea20903
Juraj Uhlar <juro.uhlar@gmail.com>
2024-09-19 18:03:05 +0100

    Chore: Migrate credential stuffing to `app` INTER-911 (#158)
    * chore: move credential stuffing client to `app`
    
    * chore: move credential stuffing server to `app`

---
+++ src/app/credential-stuffing/embed/page.tsx
@@ -0,0 +1,9 @@
+import { USE_CASES } from '../../../client/components/common/content';
+import { generateUseCaseMetadata } from '../../../client/components/common/seo';
+import { CredentialStuffing } from '.././CredentialStuffing';
+
+export const metadata = generateUseCaseMetadata(USE_CASES.credentialStuffing);
+
+export default function CredentialStuffingPage() {
+  return <CredentialStuffing />;
+}
