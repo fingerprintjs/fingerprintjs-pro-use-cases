513dd66029855f48ebc03b6776627a4190cf4d2e
Juraj Uhlar <juro.uhlar@gmail.com>
2024-10-07 21:48:48 +0200

    chore: consolidate layour

--- src/app/appLayout.tsx
+++ src/app/appLayout.tsx
@@ -1,9 +1,22 @@
 'use client';
 
+import '../styles/global-styles.scss';
 import { useSelectedLayoutSegments } from 'next/navigation';
-import { Layout } from '../Layout';
+import { IS_PRODUCTION } from '../envShared';
+import Header from '../client/components/common/Header/Header';
+import { Analytics } from '../client/analytics/Analytics';
+import Footer from '../client/components/common/Footer/Footer';
+import styles from '../styles/layout.module.scss';
 
 export default function LayoutUiInsideApp({ children }: { children: React.ReactNode }) {
   const segments = useSelectedLayoutSegments();
-  return <Layout embed={Boolean(segments?.includes('embed'))}>{children}</Layout>;
+  const embed = Boolean(segments?.includes('embed'));
+  return (
+    <div className={styles.layout}>
+      {embed ? null : <Header />}
+      {IS_PRODUCTION ? <Analytics /> : null}
+      <div>{children}</div>
+      {embed ? null : <Footer />}
+    </div>
+  );
 }
