a1dc6b6294ba1667934c2d6506d9ce78ffe3bb4a
Juraj Uhlar <juro.uhlar@gmail.com>
2025-06-20 15:23:02 +0100

    refactor: rename query parameter and add useEffect for search history refetch

--- src/app/personalization/hooks/use-products.ts
+++ src/app/personalization/hooks/use-products.ts
@@ -3,15 +3,17 @@ import { GetProductsResponse, GetProductsPayload } from '../../../app/personaliz
 import { useVisitorData } from '@fingerprintjs/fingerprintjs-pro-react';
 import { FPJS_CLIENT_TIMEOUT } from '../../../const';
 import { SEARCH_HISTORY_QUERY } from './use-search-history';
+import { useEffect } from 'react';
 
 export const GET_PRODUCTS_QUERY = 'GET_PRODUCTS_QUERY';
 
-export function useProducts(query: string) {
+export function useProducts(search: string) {
   const { data: visitorData } = useVisitorData({ timeout: FPJS_CLIENT_TIMEOUT });
   const queryClient = useQueryClient();
+
   const queryResult = useQuery<GetProductsResponse>({
     // Make a new request every time `query` changes
-    queryKey: [GET_PRODUCTS_QUERY, query],
+    queryKey: [GET_PRODUCTS_QUERY, search],
     queryFn: async () => {
       if (!visitorData) {
         throw new Error('Visitor data is undefined');
@@ -20,17 +22,24 @@ export function useProducts(query: string) {
       const response = await fetch('/personalization/api/get-products', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ requestId, query } satisfies GetProductsPayload),
+        body: JSON.stringify({ requestId, query: search } satisfies GetProductsPayload),
       });
       const result = (await response.json()) as GetProductsResponse;
       // If the query was saved, refetch the search history to show it in the "Last searches" UI
       if (result.data.querySaved) {
-        queryClient.refetchQueries({ queryKey: [SEARCH_HISTORY_QUERY] });
       }
       return result;
     },
     enabled: Boolean(visitorData),
   });
+
+  // onSuccess effect handler
+  const { data, isSuccess } = queryResult;
+  useEffect(() => {
+    if (isSuccess && data.data.querySaved) {
+      queryClient.refetchQueries({ queryKey: [SEARCH_HISTORY_QUERY] });
+    }
+  }, [isSuccess, data, queryClient]);
 
   return queryResult;
 }
