5204418dbf9b085bb8eaf29e029470457dd8d797
Juraj Uhlar <juro.uhlar@gmail.com>
2025-07-11 15:58:38 +0100

    feat: demo linked ID using session ID

--- src/app/playground/hooks/usePlaygroundSignals.ts
+++ src/app/playground/hooks/usePlaygroundSignals.ts
@@ -1,17 +1,33 @@
 import { useVisitorData } from '@fingerprintjs/fingerprintjs-pro-react';
-import { EventResponse } from '@fingerprintjs/fingerprintjs-pro-server-api';
+import { EventsGetResponse } from '@fingerprintjs/fingerprintjs-pro-server-api';
 import { useQuery, keepPreviousData } from '@tanstack/react-query';
 import { FPJS_CLIENT_TIMEOUT } from '../../../const';
 import { useEffect } from 'react';
 import { useCallbackRef } from '../../../client/hooks/useCallbackRef';
+import { useSessionId } from '../../../client/hooks/useSessionId';
 
 export function usePlaygroundSignals(config?: { onServerApiSuccess?: (data: EventResponse) => void }) {
+  const sessionId = useSessionId();
+
   const {
     data: agentResponse,
     isLoading: isLoadingAgentResponse,
     getData: getAgentData,
     error: agentError,
-  } = useVisitorData({ extendedResult: true, ignoreCache: true, timeout: FPJS_CLIENT_TIMEOUT }, { immediate: true });
+  } = useVisitorData(
+    {
+      extendedResult: true,
+      ignoreCache: true,
+      timeout: FPJS_CLIENT_TIMEOUT,
+      /**
+       * Use a simple session ID to demonstrate linkedId functionality and linked-ID-based velocity signals
+       * https://dev.fingerprint.com/docs/tagging-information#using-linkedid-and-tag-on-the-client
+       * https://dev.fingerprint.com/docs/smart-signals-reference#velocity-signals
+       */
+      linkedId: sessionId,
+    },
+    { immediate: true },
+  );
 
   const requestId = agentResponse?.requestId;
 
@@ -20,7 +36,7 @@ export function usePlaygroundSignals(config?: { onServerApiSuccess?: (data: Even
     isPending: isPendingServerResponse,
     isSuccess: isSuccessServerResponse,
     error: serverError,
-  } = useQuery<EventResponse | undefined>({
+  } = useQuery<EventsGetResponse | undefined>({
     queryKey: [requestId],
     queryFn: async () => {
       const res = await fetch(`/api/event/${agentResponse?.requestId}`, { method: 'POST' });
