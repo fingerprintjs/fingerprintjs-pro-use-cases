78d87e6a9f8b1687cbf95b813b639ddaa169518d
Przemysław Żydek <przemyslawzydek@gmail.com>
2026-01-07 14:14:44 +0100

    feat: add Content-Security-Policy header in middleware for enhanced security

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,18 @@
-import type { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 /**
  * This middleware runs on every requests and redirects from fingerprinthub.com/* to demo.fingerprint.com/*
  */
 export function middleware(request: NextRequest) {
+  const cspHeader = `
+    worker-src blob:;
+  `;
+  // Replace newline characters and spaces
+  const contentSecurityPolicyHeaderValue = cspHeader.replace(/\s{2,}/g, ' ').trim();
+
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set('Content-Security-Policy', contentSecurityPolicyHeaderValue);
+
   // Note: We need to read the hostname from a header because request.url, request.nextURL just say "localhost" on Digital Ocean
   const host = request.headers.get('host');
   if (host === 'fingerprinthub.com') {
@@ -11,5 +20,13 @@ export function middleware(request: NextRequest) {
     newURL.search = request.nextUrl.search;
     return Response.redirect(newURL, 301);
   }
-  return undefined;
+
+  const response = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
+  response.headers.set('Content-Security-Policy', contentSecurityPolicyHeaderValue);
+
+  return response;
 }
