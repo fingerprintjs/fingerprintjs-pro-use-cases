1d2efc649ccffa2e6b8c02ef0cb1398201ecf55d
Przemysław Żydek <przemyslawzydek@gmail.com>
2026-01-08 15:03:45 +0100

    chore: revert CSP bypass

--- playwright.config.ts
+++ playwright.config.ts
@@ -77,7 +77,6 @@ export default defineConfig({
       name: 'chromium',
       use: {
         ...devices['Desktop Chrome'],
-        bypassCSP: true,
         viewport: VIEWPORT,
         permissions: ['clipboard-read'],
       },
@@ -86,7 +85,6 @@ export default defineConfig({
       name: 'firefox',
       use: {
         ...devices['Desktop Firefox'],
-        bypassCSP: true,
         viewport: VIEWPORT,
         // Firefox is extra secure, so you need to enable clipboard read permission like this
         // https://github.com/microsoft/playwright/issues/13037#issuecomment-1739856724
@@ -102,7 +100,6 @@ export default defineConfig({
       name: 'webkit',
       use: {
         ...devices['Desktop Safari'],
-        bypassCSP: true,
         viewport: VIEWPORT,
       },
 
