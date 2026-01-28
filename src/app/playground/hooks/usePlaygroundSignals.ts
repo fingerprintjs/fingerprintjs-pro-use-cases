bc9635bf25b4451e2f8ab0ab81b094d2ffd30729
Przemysław Żydek <przemyslawzydek@gmail.com>
2026-01-28 11:23:40 +0100

    refactor: simplify useVisitorData options handling in usePlaygroundSignals

--- src/app/playground/hooks/usePlaygroundSignals.ts
+++ src/app/playground/hooks/usePlaygroundSignals.ts
@@ -1,7 +1,7 @@
 import { useVisitorData } from '@fingerprint/react';
 import { EventsGetResponse } from '@fingerprintjs/fingerprintjs-pro-server-api';
 import { FPJS_CLIENT_TIMEOUT } from '../../../const';
-import { useEffect, useMemo } from 'react';
+import { useEffect } from 'react';
 import { useCallbackRef } from '../../../client/hooks/useCallbackRef';
 import { useSessionId } from '../../../client/hooks/useSessionId';
 import { useEventsGetResponse } from '../../../client/hooks/useEventsGetResponse';
@@ -9,26 +9,21 @@ import { useEventsGetResponse } from '../../../client/hooks/useEventsGetResponse
 export function usePlaygroundSignals(config?: { onServerApiSuccess?: (data: EventsGetResponse) => void }) {
   const sessionId = useSessionId();
 
-  const options = useMemo(
-    () => ({
-      timeout: FPJS_CLIENT_TIMEOUT,
-      /**
-       * Use a simple session ID to demonstrate linkedId functionality and linked-ID-based velocity signals
-       * https://dev.fingerprint.com/docs/tagging-information#using-linkedid-and-tag-on-the-client
-       * https://dev.fingerprint.com/docs/smart-signals-reference#velocity-signals
-       */
-      linkedId: sessionId,
-      immediate: true,
-    }),
-    [sessionId],
-  );
-
   const {
     data: agentResponse,
     isLoading: isLoadingAgentResponse,
     getData: getAgentData,
     error: agentError,
-  } = useVisitorData(options);
+  } = useVisitorData({
+    timeout: FPJS_CLIENT_TIMEOUT,
+    /**
+     * Use a simple session ID to demonstrate linkedId functionality and linked-ID-based velocity signals
+     * https://dev.fingerprint.com/docs/tagging-information#using-linkedid-and-tag-on-the-client
+     * https://dev.fingerprint.com/docs/smart-signals-reference#velocity-signals
+     */
+    linkedId: sessionId,
+    immediate: true,
+  });
 
   const requestId = agentResponse?.event_id;
 
