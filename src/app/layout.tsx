7dbff6b383d8519bdbe07e4e97ef5bbbe5d54015
Juraj Uhlar <juro.uhlar@gmail.com>
2026-08-26 18:04:27 +0100

    refactor: nest NuqsAdapter inside client Providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import { ThirdPartyIntegrations } from '../client/thirdParty/ThirdPartyIntegrations';
 import { IS_PRODUCTION } from '../envShared';
 import Providers from './Providers';
-import { NuqsAdapter } from 'nuqs/adapters/next/app';
 
 export const metadata = {
   title: 'Fingerprint Pro Use Cases',
@@ -13,12 +12,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='en'>
       <body>
-        <NuqsAdapter>
-          <Providers>
-            {IS_PRODUCTION ? <ThirdPartyIntegrations /> : null}
-            {children}
-          </Providers>
-        </NuqsAdapter>
+        <Providers>
+          {IS_PRODUCTION ? <ThirdPartyIntegrations /> : null}
+          {children}
+        </Providers>
       </body>
     </html>
   );
